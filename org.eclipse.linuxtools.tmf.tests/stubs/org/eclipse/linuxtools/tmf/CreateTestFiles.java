4c5b8099d543ea5a3bd12c3dcf457fb2d495b0a8
Francois Chouinard <fchouinard@gmail.com>
2009-08-25 13:49:07 +0000

    Patch for Bug287488 (test plugin for TMF)

--- org.eclipse.linuxtools.tmf.tests/stubs/org/eclipse/linuxtools/tmf/CreateTestFiles.java
+++ org.eclipse.linuxtools.tmf.tests/stubs/org/eclipse/linuxtools/tmf/CreateTestFiles.java
@@ -7,18 +7,16 @@
  * http://www.eclipse.org/legal/epl-v10.html
  * 
  * Contributors:
- *   Francois Chouinard - Initial API and implementation
+ *   Francois Chouinard (fchouinard@gmail.com) - Initial API and implementation
  *******************************************************************************/
 
 package org.eclipse.linuxtools.tmf;
 
 import java.io.BufferedOutputStream;
 import java.io.DataOutputStream;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Random;
 
 /**
  * <b><u>CreateTestFiles</u></b>
@@ -38,14 +36,11 @@ public class CreateTestFiles {
     // Constants
     // ========================================================================
 
-	private static final String DIRECTORY = "testfiles";
-//	private static final String FILE_NAMES[] = { "Test-10", "Test-1K", "Test-10K", "Test-100K" };
-//    private static final int    FILE_SIZES[] = {       10 ,     1000 ,     10000 ,     100000  };
-	private static final String FILE_NAMES[] = { "Test-10K" };
-    private static final int    FILE_SIZES[] = {     10000  };
+	private static final String FILE_NAMES[] = { "Test-1K", "Test-10K", "Test-100K", "Test-1M"};
+    private static final int    FILE_SIZES[] = {  1000,      10000,      100000,      1000000 };
 
-    private static final int NB_SOURCES = 15;  
-    private static final int NB_TYPES   =  7;  
+    private static final int NB_SOURCES = 3;  
+    private static final int NB_TYPES   = 5;  
 
     // ========================================================================
     // Constructors
@@ -55,23 +50,13 @@ public class CreateTestFiles {
      * @param args
      */
     public static void main(String[] args) {
-        
-        try {
-            System.out.println("Creating test files in directory: " + new File(".").getCanonicalPath() + File.separator + DIRECTORY);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
         for (int i = 0; i < FILE_SIZES.length; i++) {
             try {
-            	createTestFile("testfiles" + File.separator + "O-" + FILE_NAMES[i], FILE_SIZES[i], true,  true);
-            	createTestFile("testfiles" + File.separator + "E-" + FILE_NAMES[i], FILE_SIZES[i], true,  false);
-                createTestFile("testfiles" + File.separator + "R-" + FILE_NAMES[i], FILE_SIZES[i], false, false);
+                createFile(FILE_NAMES[i], FILE_SIZES[i]);
             } catch (Exception e) {
             }
         }
-
-		System.out.println("Done.");
+        System.out.println("Done.");
     }
 
     // ========================================================================
@@ -81,25 +66,21 @@ public class CreateTestFiles {
     /**
      * @param file
      * @param size
-     * @param monotonic
      * @throws FileNotFoundException
      * @throws IOException
      */
-    private static void createTestFile(String file, int size, boolean monotonic, boolean odd) throws FileNotFoundException, IOException {
+    private static void createFile(String file, int size) throws FileNotFoundException, IOException {
         DataOutputStream out;
         System.out.println("Creating " + file);
         out = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(file)));
 
-        Random generator = new Random(19580427 + size);
-        long ts = (monotonic && odd) ? -1 : 0;
         for (int i = 0; i < size; i++) {
-            ts += monotonic ? 2 : generator.nextInt(10);
             int sourceIndex = i % NB_SOURCES;
             int typeIndex   = i % NB_TYPES;
-            out.writeLong(ts);                      // Timestamp
+            out.writeLong(i);                       // Timestamp
             out.writeUTF("Source-" + sourceIndex);  // Source
             out.writeUTF("Type-"   + typeIndex);    // Type
-            out.writeInt(i + 1);                    // Reference (event #)
+            out.writeInt(i);                        // Reference
             for (int j = 0; j < typeIndex; j++) {
                 out.writeUTF("Field-" + sourceIndex + "-" + j);
             }
@@ -107,5 +88,4 @@ public class CreateTestFiles {
         out.flush();
         out.close();
     }
-
 }
