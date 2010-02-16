8ca8d6e768c6b6ab3b8e9a0a6b04ad54342a2c07
William Bourque <william.bourque@polymtl.ca>
2010-02-16 20:45:51 +0000

    Svn bug (??) committed additionnal files, deleting them...

--- org.eclipse.linuxtools.lttng.jni/src/org/eclipse/linuxtools/lttng/jni_v2_5/Jni_C_Common.java
+++
@@ -1,9 +0,0 @@
-package org.eclipse.linuxtools.lttng.jni_v2_5;
-
-public class Jni_C_Common extends org.eclipse.linuxtools.lttng.jni.Jni_C_Common {
-
-	static {
-		System.loadLibrary("lttvtraceread2.5");
-	}
-
-}
--- org.eclipse.linuxtools.lttng.jni/src/org/eclipse/linuxtools/lttng/jni_v2_5/Jni_C_Pointer.java
+++
@@ -1,9 +0,0 @@
-package org.eclipse.linuxtools.lttng.jni_v2_5;
-
-public class Jni_C_Pointer extends org.eclipse.linuxtools.lttng.jni.Jni_C_Pointer {
-
-	static {
-		System.loadLibrary("lttvtraceread2.5");
-	}
-
-}
