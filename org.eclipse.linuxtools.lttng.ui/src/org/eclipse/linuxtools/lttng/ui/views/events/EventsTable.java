62b7e398a9c40ba2cee04c49235170f7a7bfe05d
Francois Chouinard <fchouinard@gmail.com>
2011-09-09 16:47:52 -0400

    Align Events table column with domain terminology

--- org.eclipse.linuxtools.lttng.ui/src/org/eclipse/linuxtools/lttng/ui/views/events/EventsTable.java
+++ org.eclipse.linuxtools.lttng.ui/src/org/eclipse/linuxtools/lttng/ui/views/events/EventsTable.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2010 Ericsson
+ * Copyright (c) 2009, 2010, 2011 Ericsson
  * 
  * All rights reserved. This program and the accompanying materials are
  * made available under the terms of the Eclipse Public License v1.0 which
@@ -8,6 +8,7 @@
  * 
  * Contributors:
  *   Francois Chouinard - Initial API and implementation
+ *   Francois Chouinard - Aligned columns with domain terminology
  *******************************************************************************/
 
 package org.eclipse.linuxtools.lttng.ui.views.events;
@@ -28,24 +29,21 @@ public class EventsTable extends TmfEventsTable {
 
     // Table column names
     static private final String TIMESTAMP_COLUMN = Messages.EventsTable_timestampColumn;
-    static private final String SOURCE_COLUMN    = Messages.EventsTable_sourceColumn;
-    static private final String TYPE_COLUMN      = Messages.EventsTable_typeColumn;
-    static private final String REFERENCE_COLUMN = Messages.EventsTable_referenceColumn;
-    static private final String CONTENT_COLUMN   = Messages.EventsTable_contentColumn;
-    static private final String[] COLUMN_NAMES =  new String[] {
-        TIMESTAMP_COLUMN,
-        SOURCE_COLUMN,
-        TYPE_COLUMN,
-        REFERENCE_COLUMN,
-        CONTENT_COLUMN
+    static private final String TRACE_COLUMN = Messages.EventsTable_traceColumn;
+    static private final String MARKER_COLUMN = Messages.EventsTable_markerColumn;
+    static private final String CONTENT_COLUMN = Messages.EventsTable_contentColumn;
+    static private final String[] COLUMN_NAMES = new String[] {
+            TIMESTAMP_COLUMN,
+            TRACE_COLUMN,
+            MARKER_COLUMN,
+            CONTENT_COLUMN
     };
 
     static private final ColumnData[] COLUMN_DATA = new ColumnData[] {
-        new ColumnData(COLUMN_NAMES[0], 125, SWT.LEFT),
-        new ColumnData(COLUMN_NAMES[1], 100, SWT.LEFT),
-        new ColumnData(COLUMN_NAMES[2], 200, SWT.LEFT),
-        new ColumnData(COLUMN_NAMES[3], 200, SWT.LEFT),
-        new ColumnData(COLUMN_NAMES[4], 100, SWT.LEFT)
+            new ColumnData(COLUMN_NAMES[0], 150, SWT.LEFT),
+            new ColumnData(COLUMN_NAMES[1], 120, SWT.LEFT),
+            new ColumnData(COLUMN_NAMES[2], 200, SWT.LEFT),
+            new ColumnData(COLUMN_NAMES[3], 100, SWT.LEFT)
     };
 
     // ------------------------------------------------------------------------
@@ -55,10 +53,9 @@ public class EventsTable extends TmfEventsTable {
     public EventsTable(Composite parent, int cacheSize) {
         super(parent, cacheSize, COLUMN_DATA);
         fTable.getColumns()[0].setData(Key.FIELD_ID, TmfEventContent.FIELD_ID_TIMESTAMP);
-        fTable.getColumns()[1].setData(Key.FIELD_ID, TmfEventContent.FIELD_ID_SOURCE);
+        fTable.getColumns()[1].setData(Key.FIELD_ID, TmfEventContent.FIELD_ID_REFERENCE);
         fTable.getColumns()[2].setData(Key.FIELD_ID, TmfEventContent.FIELD_ID_TYPE);
-        fTable.getColumns()[3].setData(Key.FIELD_ID, TmfEventContent.FIELD_ID_REFERENCE);
-        fTable.getColumns()[4].setData(Key.FIELD_ID, TmfEventContent.FIELD_ID_CONTENT);
+        fTable.getColumns()[3].setData(Key.FIELD_ID, TmfEventContent.FIELD_ID_CONTENT);
     }
 
     /**
@@ -68,15 +65,14 @@ public class EventsTable extends TmfEventsTable {
     @Override
     protected String[] extractItemFields(TmfEvent event) {
         String[] fields = new String[0];
-        
+
         if (event != null) {
             fields = new String[] {
                     event.getTimestamp().toString(),
-                    event.getSource().toString(),
-                    event.getType().toString(),
                     event.getReference().toString(),
-                    ((LttngEventContent)event.getContent()).toString() 
-                };
+                    event.getType().toString(),
+                    ((LttngEventContent) event.getContent()).toString()
+            };
         }
         return fields;
     }
