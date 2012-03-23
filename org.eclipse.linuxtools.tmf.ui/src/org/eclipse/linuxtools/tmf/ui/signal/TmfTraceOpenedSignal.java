1b5ad3d908fe0967df5cc8394c98a4ef8cea64cc
Francois Chouinard <fchouinard@gmail.com>
2012-03-23 13:31:35 -0400

    Expose more tmf.ui.signal

---
+++ org.eclipse.linuxtools.tmf.ui/src/org/eclipse/linuxtools/tmf/ui/signal/TmfTraceOpenedSignal.java
@@ -0,0 +1,52 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Ericsson
+ * 
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *   Patrick Tasse - Initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.linuxtools.tmf.ui.signal;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.linuxtools.tmf.core.signal.TmfSignal;
+import org.eclipse.linuxtools.tmf.core.trace.ITmfTrace;
+import org.eclipse.linuxtools.tmf.ui.viewers.events.ITmfEventsFilterProvider;
+
+/**
+ * <b><u>TmfTraceOpenedSignal</u></b>
+ */
+public class TmfTraceOpenedSignal extends TmfSignal {
+
+    private final ITmfTrace<?> fTrace;
+    private final IFile fBookmarksFile;
+    private final ITmfEventsFilterProvider fEventsFilterProvider;
+    
+    public TmfTraceOpenedSignal(Object source, ITmfTrace<?> trace, IFile bookmarksFile, ITmfEventsFilterProvider eventsFilterProvider) {
+        super(source);
+        fTrace = trace;
+        fBookmarksFile = bookmarksFile;
+        fEventsFilterProvider = eventsFilterProvider;
+    }
+
+    public ITmfTrace<?> getTrace() {
+        return fTrace;
+    }
+
+    public IFile getBookmarksFile() {
+        return fBookmarksFile;
+    }
+
+    public ITmfEventsFilterProvider getEventsFilterProvider() {
+        return fEventsFilterProvider;
+    }
+
+    @Override
+    public String toString() {
+        return "[TmfTraceOpenedSignal (" + fTrace.getName() + ")]"; //$NON-NLS-1$ //$NON-NLS-2$
+    }
+}
