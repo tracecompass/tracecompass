6752f420492048b8da158c9a3c316cc318bfd61d
Alexandre Montplaisir <alexmonthy@voxpopuli.im>
2012-04-27 13:54:00 -0400

    lttng: Handle existing but invalid state index files
    If a ".ht" file already exists we try to re-use it first. But if
    this step fails, now we'll just scrap the existing file and build
    a new one.
    
    Other than the many possible error causes, this will be useful
    for dealing with incompatible changes to the file format, for example.
    
    Signed-off-by: Alexandre Montplaisir <alexmonthy@voxpopuli.im>

--- org.eclipse.linuxtools.lttng2.kernel.core/src/org/eclipse/linuxtools/lttng2/kernel/core/trace/CtfKernelTrace.java
+++ org.eclipse.linuxtools.lttng2.kernel.core/src/org/eclipse/linuxtools/lttng2/kernel/core/trace/CtfKernelTrace.java
@@ -21,6 +21,7 @@ import org.eclipse.linuxtools.tmf.core.ctfadaptor.CtfTmfTrace;
 import org.eclipse.linuxtools.tmf.core.exceptions.TmfTraceException;
 import org.eclipse.linuxtools.tmf.core.statesystem.StateHistorySystem;
 import org.eclipse.linuxtools.tmf.core.statesystem.backend.historytree.HistoryTreeBackend;
+import org.eclipse.linuxtools.tmf.core.statesystem.backend.historytree.ThreadedHistoryTreeBackend;
 import org.eclipse.linuxtools.tmf.core.statesystem.helpers.HistoryBuilder;
 import org.eclipse.linuxtools.tmf.core.statesystem.helpers.IStateChangeInput;
 import org.eclipse.linuxtools.tmf.core.statesystem.helpers.IStateHistoryBackend;
@@ -33,6 +34,9 @@ import org.eclipse.linuxtools.tmf.core.statesystem.helpers.IStateHistoryBackend;
  * 
  */
 public class CtfKernelTrace extends CtfTmfTrace {
+
+    /** Size of the blocking queue to use when building a state history */
+    private final static int QUEUE_SIZE = 10000;
 
     public CtfKernelTrace() {
         super();
@@ -60,23 +64,38 @@ public class CtfKernelTrace extends CtfTmfTrace {
         /* If the target file already exists, do not rebuild it uselessly */
         // TODO for now we assume it's complete. Might be a good idea to check
         // at least if its range matches the trace's range.
-        try {
-            if (htFile.exists()) {
-                /* Load an existing history */
+        if (htFile.exists()) {
+            /* Load an existing history */
+            try {
                 htBackend = new HistoryTreeBackend(htFile);
                 this.ss = new StateHistorySystem(htBackend, false);
-            } else {
-                /* Create a new state history from scratch */
-                htInput = new CtfKernelStateInput(this);
-                htBackend = new HistoryTreeBackend(htFile,
-                        htInput.getStartTime());
-                builder = new HistoryBuilder(htInput, htBackend);
-
-                // TODO this is blocking for now...
-                builder.run();
+                return;
+            } catch (IOException e) {
+                /*
+                 * There was an error opening the existing file. Perhaps it was
+                 * corrupted, perhaps it's an old version? We'll just
+                 * fall-through and try to build a new one from scratch instead.
+                 */
             }
+        }
+
+        /* Create a new state history from scratch */
+        htInput = new CtfKernelStateInput(this);
+
+        try {
+            htBackend = new ThreadedHistoryTreeBackend(htFile,
+                    htInput.getStartTime(), QUEUE_SIZE);
+            builder = new HistoryBuilder(htInput, htBackend);
         } catch (IOException e) {
+            /* 
+             * If it fails here however, it means there was a problem writing
+             * to the disk, so throw a real exception this time.
+             */
             throw new TmfTraceException(e.getMessage());
         }
+
+        // TODO this part is blocking for now...
+        builder.run();
+
     }
 }
