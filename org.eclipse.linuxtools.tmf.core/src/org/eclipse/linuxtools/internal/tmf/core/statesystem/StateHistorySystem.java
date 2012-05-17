08aaa754d6497ee57da0f778f58279a474c57454
Alexandre Montplaisir <alexmonthy@voxpopuli.im>
2012-05-17 11:30:58 -0400

    tmf: Make sure the resolution in range queries is valid
    Signed-off-by: Alexandre Montplaisir <alexmonthy@voxpopuli.im>

--- org.eclipse.linuxtools.tmf.core/src/org/eclipse/linuxtools/internal/tmf/core/statesystem/StateHistorySystem.java
+++ org.eclipse.linuxtools.tmf.core/src/org/eclipse/linuxtools/internal/tmf/core/statesystem/StateHistorySystem.java
@@ -221,7 +221,7 @@ public class StateHistorySystem extends StateSystem implements
         long ts;
 
         /* Make sure the time range makes sense */
-        if (t2 <= t1) {
+        if (t2 <= t1 || resolution <= 0) {
             throw new TimeRangeException();
         }
 
