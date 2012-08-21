77ef700d1a3ab5fa9723ca10451e1df5379bef4e
Matthew Khouzam <matthew.khouzam@ericsson.com>
2012-08-21 16:33:18 -0400

    Fix NPE with traces that are time scaled.
    Change-Id: I733ccd4e29c42fb3239c25f5e8dc6f7959000e13
    Signed-off-by: Matthew Khouzam <matthew.khouzam@ericsson.com>
    Reviewed-on: https://git.eclipse.org/r/7337
    Reviewed-by: Alexandre Montplaisir <alexmonthy@voxpopuli.im>
    IP-Clean: Alexandre Montplaisir <alexmonthy@voxpopuli.im>
    Tested-by: Alexandre Montplaisir <alexmonthy@voxpopuli.im>
    Reviewed-by: Patrick Tasse <patrick.tasse@gmail.com>
    IP-Clean: Patrick Tasse <patrick.tasse@gmail.com>
    Tested-by: Patrick Tasse <patrick.tasse@gmail.com>

--- org.eclipse.linuxtools.tmf.core/src/org/eclipse/linuxtools/tmf/core/ctfadaptor/CtfIterator.java
+++ org.eclipse.linuxtools.tmf.core/src/org/eclipse/linuxtools/tmf/core/ctfadaptor/CtfIterator.java
@@ -122,7 +122,7 @@ public class CtfIterator extends CTFTraceReader implements ITmfContext,
 
         /* Adjust the timestamp depending on the trace's offset */
         long currTimestamp = ctfLocationData.getTimestamp();
-        final long offsetTimestamp = currTimestamp - this.getTrace().getOffset();
+        final long offsetTimestamp = this.getCtfTmfTrace().getCTFTrace().timestampNanoToCycles(currTimestamp);
         if (offsetTimestamp < 0) {
             ret = super.seek(0L);
         } else {
@@ -133,17 +133,21 @@ public class CtfIterator extends CTFTraceReader implements ITmfContext,
          * Check if there is already one or more events for that timestamp, and
          * assign the location index correctly
          */
-        currTimestamp = this.getCurrentEvent().getTimestampValue();
         long index = 0;
-        for (long i = 0; i < ctfLocationData.getIndex(); i++) {
-            if (currTimestamp == this.getCurrentEvent().getTimestampValue()) {
-                index++;
-            } else {
-                index = 0;
-            }
-            this.advance();
-        }
+        if (this.getCurrentEvent() != null) {
+            currTimestamp = this.getCurrentEvent().getTimestampValue();
 
+            for (long i = 0; i < ctfLocationData.getIndex(); i++) {
+                if (currTimestamp == this.getCurrentEvent().getTimestampValue()) {
+                    index++;
+                } else {
+                    index = 0;
+                }
+                this.advance();
+            }
+        } else {
+            ret= false;
+        }
         /* Seek the current location accordingly */
         if (ret) {
             curLocation.setLocation(new CtfLocationData(getCurrentEvent().getTimestampValue(), index));
