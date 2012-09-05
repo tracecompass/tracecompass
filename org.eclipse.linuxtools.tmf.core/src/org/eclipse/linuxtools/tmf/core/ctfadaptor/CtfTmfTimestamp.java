bd3982fbb6413b79299dee65d7d4e701e87eb657
Patrick Tasse <patrick.tasse@gmail.com>
2012-09-05 13:26:06 -0400

    Add thousands space separator for nanosecond timestamp output.
    Change-Id: I76de73c1cc9ce0319b97a0be9b2cc8ca454e1615
    Reviewed-on: https://git.eclipse.org/r/7623
    Tested-by: Hudson CI
    Reviewed-by: Bernd Hufmann <bhufmann@gmail.com>
    IP-Clean: Bernd Hufmann <bhufmann@gmail.com>

--- org.eclipse.linuxtools.tmf.core/src/org/eclipse/linuxtools/tmf/core/ctfadaptor/CtfTmfTimestamp.java
+++ org.eclipse.linuxtools.tmf.core/src/org/eclipse/linuxtools/tmf/core/ctfadaptor/CtfTmfTimestamp.java
@@ -151,8 +151,15 @@ public class CtfTmfTimestamp extends TmfTimestamp {
     private String nanoToString() {
         final long timestamp = getValue();
         StringBuilder retVal = new StringBuilder();
-        retVal.append(timestamp);
-        retVal.append(" ns"); //$NON-NLS-1$
+        String time = Long.toString(timestamp);
+        for (int i = 0; i < time.length(); i++) {
+            int pos = time.length() - i - 1;
+            retVal.append(time.charAt(i));
+            if (pos % 3 == 0) {
+                retVal.append(' ');
+            }
+        }
+        retVal.append("ns"); //$NON-NLS-1$
         return retVal.toString();
     }
 
