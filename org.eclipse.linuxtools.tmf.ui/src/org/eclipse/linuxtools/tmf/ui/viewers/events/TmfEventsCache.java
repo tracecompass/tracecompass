12bc2ed7055d31e2cb52536091e93134c2a9a950
Patrick Tasse <patrick.tasse@gmail.com>
2012-09-26 14:02:00 -0400

    Fix for bug 390472: ArrayIndexOutOfBoundsException in TmfEventsCache.
    Change-Id: I1025ec36868837a9e027399f13d02266c24a152e
    Reviewed-on: https://git.eclipse.org/r/7936
    Tested-by: Hudson CI
    Reviewed-by: Bernd Hufmann <bhufmann@gmail.com>
    IP-Clean: Bernd Hufmann <bhufmann@gmail.com>

--- org.eclipse.linuxtools.tmf.ui/src/org/eclipse/linuxtools/tmf/ui/viewers/events/TmfEventsCache.java
+++ org.eclipse.linuxtools.tmf.ui/src/org/eclipse/linuxtools/tmf/ui/viewers/events/TmfEventsCache.java
@@ -339,6 +339,9 @@ public class TmfEventsCache {
                         if (event != null) {
                             if (((fFilter == null) || fFilter.matches(event)) && (skipCount-- <= 0)) {
                                 synchronized (TmfEventsCache.this) {
+                                    if (monitor.isCanceled()) {
+                                        return;
+                                    }
                                     fCache[count] = new CachedEvent(event.clone(), rank);
                                     count++;
                                     fCacheEndIndex++;
