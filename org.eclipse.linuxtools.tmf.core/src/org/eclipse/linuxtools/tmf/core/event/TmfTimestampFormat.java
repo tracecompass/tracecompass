3d06e613662d059d3a9193583ff1343e8e2c7934
Alexandre Montplaisir <alexmonthy@voxpopuli.im>
2013-01-03 10:54:18 -0500

    tmf: Remove unused import in TmfTimestampFormat
    Change-Id: I8aa86e5f20ca620cde3492bcac3a137f5cd65d2e
    Signed-off-by: Alexandre Montplaisir <alexmonthy@voxpopuli.im>
    Reviewed-on: https://git.eclipse.org/r/9430
    Reviewed-by: Patrick Tasse <patrick.tasse@gmail.com>
    IP-Clean: Patrick Tasse <patrick.tasse@gmail.com>
    Tested-by: Hudson CI

--- org.eclipse.linuxtools.tmf.core/src/org/eclipse/linuxtools/tmf/core/event/TmfTimestampFormat.java
+++ org.eclipse.linuxtools.tmf.core/src/org/eclipse/linuxtools/tmf/core/event/TmfTimestampFormat.java
@@ -19,7 +19,6 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
-import java.util.TimeZone;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
