4962833a9e2ad1abafcb25be6171afe3d5b965e1
Jean-Christian Kouamé <kadjo.gwandy.jean-christian.kouame@ericsson.com>
2013-05-17 16:14:02 -0400

    tmf: Show trace environment variables in the Properties view
    Fixes bug 407756
    
    Change-Id: Ie90a3094ea8adc335a82182127d7bc0e3b727616
    Signed-off-by: Jean-Christian Kouame
    Reviewed-on: https://git.eclipse.org/r/12829
    Tested-by: Hudson CI
    Reviewed-by: Alexandre Montplaisir <alexmonthy@voxpopuli.im>
    IP-Clean: Alexandre Montplaisir <alexmonthy@voxpopuli.im>
    Tested-by: Alexandre Montplaisir <alexmonthy@voxpopuli.im>

---
+++ org.eclipse.linuxtools.tmf.ui/src/org/eclipse/linuxtools/tmf/ui/project/model/Messages.java
@@ -0,0 +1,55 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Ericsson
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Jean-Christian Kouamé - Initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.linuxtools.tmf.ui.project.model;
+
+import org.eclipse.osgi.util.NLS;
+
+/**
+ * Message strings for TMF model handling.
+ *
+ * @author Jean-Christian Kouamé
+ * @since 2.1
+ */
+public class Messages extends NLS {
+
+    private static final String BUNDLE_NAME = "org.eclipse.linuxtools.tmf.ui.project.model.messages"; //$NON-NLS-1$
+
+    /** The category of the resource properties */
+    public static String TmfTraceElement_ResourceProperties;
+
+    /** The category of the trace properties */
+    public static String TmfTraceElement_TraceProperties;
+
+    /** The descriptor for the name property */
+    public static String TmfTraceElement_Name;
+
+    /** The descriptor for the path property */
+    public static String TmfTraceElement_Path;
+
+    /** The descriptor for the location properties */
+    public static String TmfTraceElement_Location;
+
+    /** The descriptor for the event type property */
+    public static String TmfTraceElement_EventType;
+
+    /** The description for the linked property */
+    public static String TmfTraceElement_IsLinked;
+
+    static {
+        // initialize resource bundle
+        NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+    }
+
+    private Messages() {
+    }
+}
