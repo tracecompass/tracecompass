b518f27e672378e7540473c0b6ae9f4a84e83591
Patrick Tasse <patrick.tasse@gmail.com>
2013-05-21 12:58:45 -0400

    Log exceptions in event thread
    Fix for bug 343740.
    
    Change-Id: I554f74bdeb4b2c5de1706798633ff702d6493bbc
    Signed-off-by: Patrick Tasse <patrick.tasse@gmail.com>
    Reviewed-on: https://git.eclipse.org/r/13030
    Tested-by: Hudson CI
    Reviewed-by: Bernd Hufmann <bernd.hufmann@ericsson.com>
    IP-Clean: Bernd Hufmann <bernd.hufmann@ericsson.com>

--- org.eclipse.linuxtools.tmf.core/src/org/eclipse/linuxtools/internal/tmf/core/component/TmfEventThread.java
+++ org.eclipse.linuxtools.tmf.core/src/org/eclipse/linuxtools/internal/tmf/core/component/TmfEventThread.java
@@ -12,6 +12,7 @@
 
 package org.eclipse.linuxtools.internal.tmf.core.component;
 
+import org.eclipse.linuxtools.internal.tmf.core.Activator;
 import org.eclipse.linuxtools.internal.tmf.core.TmfCoreTracer;
 import org.eclipse.linuxtools.tmf.core.component.ITmfDataProvider;
 import org.eclipse.linuxtools.tmf.core.component.TmfDataProvider;
@@ -200,6 +201,7 @@ public class TmfEventThread implements Runnable {
             }
 
         } catch (Exception e) {
+            Activator.logError("Error in " + fProvider.getName() + " handling " + fRequest, e); //$NON-NLS-1$ //$NON-NLS-2$
             fRequest.fail();
         }
 
