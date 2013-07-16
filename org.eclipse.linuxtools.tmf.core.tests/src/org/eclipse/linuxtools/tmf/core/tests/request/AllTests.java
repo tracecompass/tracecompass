3d3ae98082a8cdcc8dda282e1604b15d70e6dba3
Alexandre Montplaisir <alexmonthy@voxpopuli.im>
2013-07-16 13:48:57 -0400

    tmf: Remove old event requests test
    Remove an old test that was depandent on the previous scheduler
    behavior (and currently fails). The new tests introduced in cac6bb9
    cover the same use cases, so the old one is now obsolete.
    
    Change-Id: Ie7bc879599ceb981406ddf9e33729e86138bf661
    Signed-off-by: Alexandre Montplaisir <alexmonthy@voxpopuli.im>
    Reviewed-on: https://git.eclipse.org/r/14598
    Tested-by: Hudson CI
    Reviewed-by: Marc-Andre Laperle <marc-andre.laperle@ericsson.com>

--- org.eclipse.linuxtools.tmf.core.tests/src/org/eclipse/linuxtools/tmf/core/tests/request/AllTests.java
+++ org.eclipse.linuxtools.tmf.core.tests/src/org/eclipse/linuxtools/tmf/core/tests/request/AllTests.java
@@ -25,7 +25,6 @@ import org.junit.runners.Suite;
     TmfCoalescedEventRequestTest.class,
     TmfDataRequestTest.class,
     TmfEventRequestTest.class,
-    TmfRequestExecutorTest.class,
     TmfSchedulerTest.class
 })
 public class AllTests {
