c004295caa8641ff12dada7a9182c44728db9a95
Patrick Tasse <patrick.tasse@gmail.com>
2013-08-02 16:38:44 -0400

    tmf: Make time graph combo item heights dynamically linked
    The height of items in the time graph control are dynamically linked to
    the height of their corresponding tree items. This ensures alignment
    when tree items have different heights due to custom fonts or images.
    
    Change-Id: I84a2394f6d5673bec1af9a2858cc0a31a76f2753
    Signed-off-by: Patrick Tasse <patrick.tasse@gmail.com>
    Reviewed-on: https://git.eclipse.org/r/15107
    Tested-by: Hudson CI
    Reviewed-by: Bernd Hufmann <bernd.hufmann@ericsson.com>
    IP-Clean: Bernd Hufmann <bernd.hufmann@ericsson.com>

--- org.eclipse.linuxtools.tmf.ui/src/org/eclipse/linuxtools/tmf/ui/widgets/timegraph/widgets/TimeGraphControl.java
+++ org.eclipse.linuxtools.tmf.ui/src/org/eclipse/linuxtools/tmf/ui/widgets/timegraph/widgets/TimeGraphControl.java
@@ -953,20 +953,12 @@ public class TimeGraphControl extends TimeGraphBaseControl implements FocusListe
         if (y < 0) {
             return -1;
         }
-        if (fGlobalItemHeight == CUSTOM_ITEM_HEIGHT) {
-            int ySum = 0;
-            for (int idx = fTopIndex; idx < fItemData.fExpandedItems.length; idx++) {
-                ySum += fItemData.fExpandedItems[idx].fItemHeight;
-                if (y < ySum) {
-                    return idx;
-                }
+        int ySum = 0;
+        for (int idx = fTopIndex; idx < fItemData.fExpandedItems.length; idx++) {
+            ySum += fItemData.fExpandedItems[idx].fItemHeight;
+            if (y < ySum) {
+                return idx;
             }
-            return -1;
-        }
-        int idx = y / fGlobalItemHeight;
-        idx += fTopIndex;
-        if (idx < fItemData.fExpandedItems.length) {
-            return idx;
         }
         return -1;
     }
@@ -1072,26 +1064,20 @@ public class TimeGraphControl extends TimeGraphBaseControl implements FocusListe
     public int countPerPage() {
         int height = getCtrlSize().y;
         int count = 0;
-        if (fGlobalItemHeight == CUSTOM_ITEM_HEIGHT) {
-            int ySum = 0;
-            for (int idx = fTopIndex; idx < fItemData.fExpandedItems.length; idx++) {
-                ySum += fItemData.fExpandedItems[idx].fItemHeight;
-                if (ySum >= height) {
-                    return count;
-                }
-                count++;
+        int ySum = 0;
+        for (int idx = fTopIndex; idx < fItemData.fExpandedItems.length; idx++) {
+            ySum += fItemData.fExpandedItems[idx].fItemHeight;
+            if (ySum >= height) {
+                return count;
             }
-            for (int idx = fTopIndex - 1; idx >= 0; idx--) {
-                ySum += fItemData.fExpandedItems[idx].fItemHeight;
-                if (ySum >= height) {
-                    return count;
-                }
-                count++;
-            }
-            return count;
+            count++;
         }
-        if (height > 0) {
-            count = height / fGlobalItemHeight;
+        for (int idx = fTopIndex - 1; idx >= 0; idx--) {
+            ySum += fItemData.fExpandedItems[idx].fItemHeight;
+            if (ySum >= height) {
+                return count;
+            }
+            count++;
         }
         return count;
     }
@@ -1136,34 +1122,27 @@ public class TimeGraphControl extends TimeGraphBaseControl implements FocusListe
     }
 
     Rectangle getNameRect(Rectangle bound, int idx, int nameWidth) {
-        int x = bound.x;
-        int y = bound.y + (idx - fTopIndex) * fGlobalItemHeight;
-        int width = nameWidth;
-        int height = fGlobalItemHeight;
-        if (fGlobalItemHeight == CUSTOM_ITEM_HEIGHT) {
-            int ySum = 0;
-            for (int i = fTopIndex; i < idx; i++) {
-                ySum += fItemData.fExpandedItems[i].fItemHeight;
-            }
-            y = bound.y + ySum;
-            height = fItemData.fExpandedItems[idx].fItemHeight;
-        }
-        return new Rectangle(x, y, width, height);
+        Rectangle rect = getStatesRect(bound, idx, nameWidth);
+        rect.x = bound.x;
+        rect.width = nameWidth;
+        return rect;
     }
 
     Rectangle getStatesRect(Rectangle bound, int idx, int nameWidth) {
         int x = bound.x + nameWidth;
-        int y = bound.y + (idx - fTopIndex) * fGlobalItemHeight;
         int width = bound.width - x;
-        int height = fGlobalItemHeight;
-        if (fGlobalItemHeight == CUSTOM_ITEM_HEIGHT) {
-            int ySum = 0;
+        int ySum = 0;
+        if (idx >= fTopIndex) {
             for (int i = fTopIndex; i < idx; i++) {
                 ySum += fItemData.fExpandedItems[i].fItemHeight;
             }
-            y = bound.y + ySum;
-            height = fItemData.fExpandedItems[idx].fItemHeight;
+        } else {
+            for (int i = fTopIndex - 1; i >= idx; i--) {
+                ySum -= fItemData.fExpandedItems[i].fItemHeight;
+            }
         }
+        int y = bound.y + ySum;
+        int height = fItemData.fExpandedItems[idx].fItemHeight;
         return new Rectangle(x, y, width, height);
     }
 
@@ -2259,20 +2238,40 @@ public class TimeGraphControl extends TimeGraphBaseControl implements FocusListe
     }
 
     /**
-     * @return The height of regular item rows
+     * @return The default height of regular item rows
      */
     public int getItemHeight() {
         return fGlobalItemHeight;
     }
 
     /**
-     * Set the height of regular itew rows
+     * Set the default height of regular item rows.
      *
      * @param rowHeight
      *            The height
      */
     public void setItemHeight(int rowHeight) {
         this.fGlobalItemHeight = rowHeight;
+    }
+
+    /**
+     * Set the height of a specific item. Overrides the default item height.
+     *
+     * @param entry
+     *            A time graph entry
+     * @param rowHeight
+     *            The height
+     * @return true if the height is successfully stored, false otherwise
+     *
+     * @since 2.1
+     */
+    public boolean setItemHeight(ITimeGraphEntry entry, int rowHeight) {
+        Item item = fItemData.findItem(entry);
+        if (item != null) {
+            item.fItemHeight = rowHeight;
+            return true;
+        }
+        return false;
     }
 
     /**
@@ -2411,7 +2410,11 @@ public class TimeGraphControl extends TimeGraphBaseControl implements FocusListe
             if (parent != null) {
                 parent.fChildren.add(item);
             }
-            item.fItemHeight = fTimeGraphProvider.getItemHeight(entry);
+            if (fGlobalItemHeight == CUSTOM_ITEM_HEIGHT) {
+                item.fItemHeight = fTimeGraphProvider.getItemHeight(entry);
+            } else {
+                item.fItemHeight = fGlobalItemHeight;
+            }
             itemList.add(item);
             if (entry.hasChildren()) {
                 item.fExpanded = true;
