f0414df8b561a270603492d4d6ebe6e195dd9cbc
Simon Delisle <simon.delisle@ericsson.com>
2013-08-14 14:35:31 -0400

    Ctf: add removeIterator() to prevent OutOfMemory exception (Bug 414998)
    Change-Id: I0a42a1b4d7eb14b37bc97471dba1b4aae44d2f4c
    Signed-off-by: Simon Delisle <simon.delisle@ericsson.com>
    Reviewed-on: https://git.eclipse.org/r/15487
    Reviewed-by: Alexandre Montplaisir <alexmonthy@voxpopuli.im>
    IP-Clean: Alexandre Montplaisir <alexmonthy@voxpopuli.im>
    Tested-by: Alexandre Montplaisir <alexmonthy@voxpopuli.im>

--- org.eclipse.linuxtools.tmf.core/src/org/eclipse/linuxtools/tmf/core/ctfadaptor/CtfIteratorManager.java
+++ org.eclipse.linuxtools.tmf.core/src/org/eclipse/linuxtools/tmf/core/ctfadaptor/CtfIteratorManager.java
@@ -6,7 +6,9 @@
  * accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
- * Contributors: Matthew Khouzam - Initial API and implementation
+ * Contributors:
+ *   Matthew Khouzam - Initial API and implementation
+ *   Simon Delisle - Added a method to remove the iterator
  *******************************************************************************/
 package org.eclipse.linuxtools.tmf.core.ctfadaptor;
 
@@ -70,6 +72,22 @@ public abstract class CtfIteratorManager {
     public static synchronized CtfIterator getIterator(final CtfTmfTrace trace,
             final CtfTmfContext ctx) {
         return map.get(trace).getIterator(ctx);
+    }
+
+    /**
+     * Remove an iterator for a given trace and context
+     *
+     * @param trace
+     *            the trace
+     * @param ctx
+     *            the context
+     * @since 2.1
+     */
+    public static synchronized void removeIterator(final CtfTmfTrace trace, final CtfTmfContext ctx) {
+        CtfTraceManager traceManager = map.get(trace);
+        if (traceManager != null) {
+            traceManager.removeIterator(ctx);
+        }
     }
 }
 
@@ -152,6 +170,11 @@ class CtfTraceManager {
             }
         }
         return retVal;
+    }
+
+    public void removeIterator(CtfTmfContext context) {
+        fMap.remove(context);
+        fRandomAccess.remove(context);
     }
 
     /**
