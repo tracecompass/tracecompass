b2b36236523d1ab3e6798f4ec75b56656147206e
Geneviève Bastien <gbastien+lttng@versatic.net>
2013-11-25 14:27:57 -0500

    TMF: Check presence of fields for TCP event synchronization
    Traces using an old version of the TCP addons module would get a NPE when
    getting the key to match packets.
    
    Change-Id: Ib0470834807b1933f5f46069c5377115764b9dda
    Signed-off-by: Geneviève Bastien <gbastien+lttng@versatic.net>
    Reviewed-on: https://git.eclipse.org/r/18848
    Tested-by: Hudson CI
    Reviewed-by: Alexandre Montplaisir <alexmonthy@voxpopuli.im>
    IP-Clean: Alexandre Montplaisir <alexmonthy@voxpopuli.im>

--- org.eclipse.linuxtools.lttng2.kernel.core/src/org/eclipse/linuxtools/lttng2/kernel/core/event/matching/TcpEventMatching.java
+++ org.eclipse.linuxtools.lttng2.kernel.core/src/org/eclipse/linuxtools/lttng2/kernel/core/event/matching/TcpEventMatching.java
@@ -18,9 +18,10 @@ import java.util.List;
 import org.eclipse.linuxtools.internal.lttng2.kernel.core.TcpEventStrings;
 import org.eclipse.linuxtools.tmf.core.ctfadaptor.CtfTmfTrace;
 import org.eclipse.linuxtools.tmf.core.event.ITmfEvent;
+import org.eclipse.linuxtools.tmf.core.event.ITmfEventField;
+import org.eclipse.linuxtools.tmf.core.event.matching.ITmfNetworkMatchDefinition;
 import org.eclipse.linuxtools.tmf.core.event.matching.TmfEventMatching.MatchingType;
 import org.eclipse.linuxtools.tmf.core.event.matching.TmfNetworkEventMatching.Direction;
-import org.eclipse.linuxtools.tmf.core.event.matching.ITmfNetworkMatchDefinition;
 import org.eclipse.linuxtools.tmf.core.trace.ITmfTrace;
 
 /**
@@ -37,9 +38,24 @@ import org.eclipse.linuxtools.tmf.core.trace.ITmfTrace;
  */
 public class TcpEventMatching implements ITmfNetworkMatchDefinition {
 
+    private static boolean canMatchPacket(final ITmfEvent event) {
+        /* Make sure all required fields are present to match with this event */
+        ITmfEventField content = event.getContent();
+        if ((content.getField(TcpEventStrings.SEQ) != null) &&
+                (content.getField(TcpEventStrings.ACKSEQ) != null) && (content.getField(TcpEventStrings.FLAGS) != null)) {
+            return true;
+        }
+        return false;
+    }
+
     @Override
     public Direction getDirection(ITmfEvent event) {
         String evname = event.getType().getName();
+
+        if (!canMatchPacket(event)) {
+            return null;
+        }
+
         /* Is the event a tcp socket in or out event */
         if (evname.equals(TcpEventStrings.INET_SOCK_LOCAL_IN)) {
             return Direction.IN;
