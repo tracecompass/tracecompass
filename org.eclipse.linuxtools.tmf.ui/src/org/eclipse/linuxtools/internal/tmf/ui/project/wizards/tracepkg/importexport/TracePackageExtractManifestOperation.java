30bf6b07b0a736fde3ec31eba80bd9efa13279d1
Marc-Andre Laperle <marc-andre.laperle@ericsson.com>
2014-01-28 12:46:07 -0500

    tmf: Introduce ManifestReader
    Change-Id: I5b24d8339b64e08782c4f1c16c1115b13f1c39df
    Signed-off-by: Marc-Andre Laperle <marc-andre.laperle@ericsson.com>
    Reviewed-on: https://git.eclipse.org/r/22067
    Tested-by: Hudson CI

--- org.eclipse.linuxtools.tmf.ui/src/org/eclipse/linuxtools/internal/tmf/ui/project/wizards/tracepkg/importexport/TracePackageExtractManifestOperation.java
+++ org.eclipse.linuxtools.tmf.ui/src/org/eclipse/linuxtools/internal/tmf/ui/project/wizards/tracepkg/importexport/TracePackageExtractManifestOperation.java
@@ -12,27 +12,14 @@
 
 package org.eclipse.linuxtools.internal.tmf.ui.project.wizards.tracepkg.importexport;
 
-import java.io.IOException;
 import java.io.InputStream;
-import java.net.URL;
 import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.Enumeration;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
-import javax.xml.XMLConstants;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.stream.StreamSource;
-import javax.xml.validation.Schema;
-import javax.xml.validation.SchemaFactory;
-import javax.xml.validation.Validator;
-
-import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
@@ -42,18 +29,9 @@ import org.eclipse.jface.operation.ModalContext;
 import org.eclipse.linuxtools.internal.tmf.ui.Activator;
 import org.eclipse.linuxtools.internal.tmf.ui.project.wizards.tracepkg.AbstractTracePackageOperation;
 import org.eclipse.linuxtools.internal.tmf.ui.project.wizards.tracepkg.ITracePackageConstants;
-import org.eclipse.linuxtools.internal.tmf.ui.project.wizards.tracepkg.TracePackageBookmarkElement;
 import org.eclipse.linuxtools.internal.tmf.ui.project.wizards.tracepkg.TracePackageElement;
 import org.eclipse.linuxtools.internal.tmf.ui.project.wizards.tracepkg.TracePackageFilesElement;
-import org.eclipse.linuxtools.internal.tmf.ui.project.wizards.tracepkg.TracePackageSupplFileElement;
-import org.eclipse.linuxtools.internal.tmf.ui.project.wizards.tracepkg.TracePackageSupplFilesElement;
 import org.eclipse.linuxtools.internal.tmf.ui.project.wizards.tracepkg.TracePackageTraceElement;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NamedNodeMap;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
 
 /**
  * An operation that extracts information from the manifest located in an
@@ -62,9 +40,6 @@ import org.xml.sax.SAXException;
  * @author Marc-Andre Laperle
  */
 public class TracePackageExtractManifestOperation extends AbstractTracePackageOperation {
-
-    private static final String SCHEMA_FOLDER_NAME = "schema"; //$NON-NLS-1$
-    private static final String EXPORT_MANIFEST_SCHEMA_FILE_NAME = "export-manifest.xsd"; //$NON-NLS-1$
 
     /**
      * Constructs a new import operation for reading the manifest
@@ -110,10 +85,10 @@ public class TracePackageExtractManifestOperation extends AbstractTracePackageOp
                 if (entry.getName().endsWith(ITracePackageConstants.MANIFEST_FILENAME)) {
                     found = true;
                     InputStream inputStream = archiveFile.getInputStream(entry);
-                    validateManifest(inputStream);
+                    ManifestReader.validateManifest(inputStream);
 
                     inputStream = archiveFile.getInputStream(entry);
-                    elements = loadElementsFromManifest(inputStream);
+                    elements = ManifestReader.loadElementsFromManifest(inputStream);
                     break;
                 }
 
@@ -164,100 +139,4 @@ public class TracePackageExtractManifestOperation extends AbstractTracePackageOp
         return packageElements.toArray(new TracePackageElement[] {});
     }
 
-    private static void validateManifest(InputStream xml) throws IOException
-    {
-        URL schemaFileUrl = FileLocator.find(Activator.getDefault().getBundle(), new Path(SCHEMA_FOLDER_NAME).append(EXPORT_MANIFEST_SCHEMA_FILE_NAME), null);
-        if (schemaFileUrl == null) {
-            throw new IOException(MessageFormat.format(Messages.TracePackageExtractManifestOperation_SchemaFileNotFound, EXPORT_MANIFEST_SCHEMA_FILE_NAME));
-        }
-
-        try {
-            SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-            Schema schema = factory.newSchema(new StreamSource(schemaFileUrl.openStream()));
-            Validator validator = schema.newValidator();
-            validator.validate(new StreamSource(xml));
-        } catch (SAXException e) {
-            throw new IOException(Messages.TracePackageExtractManifestOperation_ErrorManifestNotValid, e);
-        } catch (IOException e) {
-            throw new IOException(Messages.TracePackageExtractManifestOperation_ErrorManifestNotValid, e);
-        }
-    }
-
-    private static TracePackageElement[] loadElementsFromManifest(InputStream inputStream) throws IOException, SAXException, ParserConfigurationException {
-        List<TracePackageElement> packageElements = new ArrayList<>();
-        TracePackageElement element = null;
-        Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(inputStream);
-
-        NodeList traceElements = doc.getDocumentElement().getElementsByTagName(ITracePackageConstants.TRACE_ELEMENT);
-        for (int i = 0; i < traceElements.getLength(); ++i) {
-            Node traceNode = traceElements.item(i);
-            if (traceNode.getNodeType() == Node.ELEMENT_NODE) {
-                Element traceElement = (Element) traceNode;
-                String traceName = traceElement.getAttribute(ITracePackageConstants.TRACE_NAME_ATTRIB);
-                String traceType = traceElement.getAttribute(ITracePackageConstants.TRACE_TYPE_ATTRIB);
-                element = new TracePackageTraceElement(null, traceName, traceType);
-
-                List<TracePackageElement> children = new ArrayList<>();
-                NodeList fileElements = traceElement.getElementsByTagName(ITracePackageConstants.TRACE_FILE_ELEMENT);
-                for (int j = 0; j < fileElements.getLength(); ++j) {
-                    Node fileNode = fileElements.item(j);
-                    if (fileNode.getNodeType() == Node.ELEMENT_NODE) {
-                        Element fileElement = (Element) fileNode;
-                        String fileName = fileElement.getAttribute(ITracePackageConstants.TRACE_FILE_NAME_ATTRIB);
-                        children.add(new TracePackageFilesElement(element, fileName));
-                    }
-                }
-
-                TracePackageSupplFilesElement supplFilesElement = new TracePackageSupplFilesElement(element);
-
-                // Supplementary files
-                List<TracePackageSupplFileElement> suppFiles = new ArrayList<>();
-                NodeList suppFilesElements = traceElement.getElementsByTagName(ITracePackageConstants.SUPPLEMENTARY_FILE_ELEMENT);
-                for (int j = 0; j < suppFilesElements.getLength(); ++j) {
-                    Node suppFileNode = suppFilesElements.item(j);
-                    if (suppFileNode.getNodeType() == Node.ELEMENT_NODE) {
-                        Element suppFileElement = (Element) suppFileNode;
-                        String fileName = suppFileElement.getAttribute(ITracePackageConstants.SUPPLEMENTARY_FILE_NAME_ATTRIB);
-                        TracePackageSupplFileElement supplFile = new TracePackageSupplFileElement(fileName, supplFilesElement);
-                        suppFiles.add(supplFile);
-                    }
-                }
-
-                if (!suppFiles.isEmpty()) {
-                    supplFilesElement.setChildren(suppFiles.toArray(new TracePackageElement[] {}));
-                    children.add(supplFilesElement);
-                }
-
-                // bookmarks
-                List<Map<String, String>> bookmarkAttribs = new ArrayList<>();
-                NodeList bookmarksElements = traceElement.getElementsByTagName(ITracePackageConstants.BOOKMARKS_ELEMENT);
-                for (int j = 0; j < bookmarksElements.getLength(); ++j) {
-                    Node bookmarksNode = bookmarksElements.item(j);
-                    if (bookmarksNode.getNodeType() == Node.ELEMENT_NODE) {
-                        NodeList bookmarkElements = traceElement.getElementsByTagName(ITracePackageConstants.BOOKMARK_ELEMENT);
-                        for (int k = 0; k < bookmarkElements.getLength(); ++k) {
-                            Node bookmarkNode = bookmarkElements.item(k);
-                            if (bookmarkNode.getNodeType() == Node.ELEMENT_NODE) {
-                                Element bookmarkElement = (Element) bookmarkNode;
-                                NamedNodeMap attributesMap = bookmarkElement.getAttributes();
-                                Map<String, String> attribs = new HashMap<>();
-                                for (int l = 0; l < attributesMap.getLength(); ++l) {
-                                    Node item = attributesMap.item(l);
-                                    attribs.put(item.getNodeName(), item.getNodeValue());
-                                }
-                                bookmarkAttribs.add(attribs);
-                            }
-                        }
-                    }
-                }
-                if (!bookmarkAttribs.isEmpty()) {
-                    children.add(new TracePackageBookmarkElement(element, bookmarkAttribs));
-                }
-
-                element.setChildren(children.toArray(new TracePackageElement[] {}));
-                packageElements.add(element);
-            }
-        }
-        return packageElements.toArray(new TracePackageElement[] {});
-    }
 }
