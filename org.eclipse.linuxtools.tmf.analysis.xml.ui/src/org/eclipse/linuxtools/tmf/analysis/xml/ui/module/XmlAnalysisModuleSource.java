0281ea1c63cce2a92f943a17ac196749c8ae1f1e
Geneviève Bastien <gbastien+lttng@versatic.net>
2014-03-11 13:14:57 -0400

    TMF XML: Add support of the XML file extension point to the modules source
    Change-Id: I2f651ec4e6698d5edcfa1f6f279ffa979328d545
    Signed-off-by: Geneviève Bastien <gbastien+lttng@versatic.net>
    Reviewed-on: https://git.eclipse.org/r/23212
    Tested-by: Hudson CI
    Reviewed-by: Matthew Khouzam <matthew.khouzam@ericsson.com>
    IP-Clean: Matthew Khouzam <matthew.khouzam@ericsson.com>
    Tested-by: Matthew Khouzam <matthew.khouzam@ericsson.com>

--- org.eclipse.linuxtools.tmf.analysis.xml.ui/src/org/eclipse/linuxtools/tmf/analysis/xml/ui/module/XmlAnalysisModuleSource.java
+++ org.eclipse.linuxtools.tmf.analysis.xml.ui/src/org/eclipse/linuxtools/tmf/analysis/xml/ui/module/XmlAnalysisModuleSource.java
@@ -14,6 +14,7 @@ package org.eclipse.linuxtools.tmf.analysis.xml.ui.module;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,7 +22,10 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.linuxtools.internal.tmf.analysis.xml.ui.Activator;
 import org.eclipse.linuxtools.tmf.analysis.xml.core.module.XmlUtils;
 import org.eclipse.linuxtools.tmf.analysis.xml.core.stateprovider.TmfXmlStrings;
@@ -29,6 +33,7 @@ import org.eclipse.linuxtools.tmf.analysis.xml.ui.module.TmfAnalysisModuleHelper
 import org.eclipse.linuxtools.tmf.core.analysis.IAnalysisModuleHelper;
 import org.eclipse.linuxtools.tmf.core.analysis.IAnalysisModuleSource;
 import org.eclipse.linuxtools.tmf.core.analysis.TmfAnalysisManager;
+import org.osgi.framework.Bundle;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
@@ -43,46 +48,80 @@ import org.xml.sax.SAXException;
  */
 public class XmlAnalysisModuleSource implements IAnalysisModuleSource {
 
+    /** Extension point ID */
+    private static final String TMF_XML_BUILTIN_ID = "org.eclipse.linuxtools.tmf.analysis.xml.core.files"; //$NON-NLS-1$
+    private static final String XML_FILE_ELEMENT = "xmlfile"; //$NON-NLS-1$
+
+    private static final String XML_FILE_ATTRIB = "file"; //$NON-NLS-1$
+
     private static List<IAnalysisModuleHelper> fModules = null;
 
     @Override
     public synchronized Iterable<IAnalysisModuleHelper> getAnalysisModules() {
         if (fModules == null) {
+            fModules = new ArrayList<>();
+            populateBuiltinModules();
             populateAnalysisModules();
         }
         return fModules;
     }
 
+    private static void processFile(File xmlFile) {
+        if (!XmlUtils.xmlValidate(xmlFile).isOK()) {
+            return;
+        }
+
+        try {
+            /* Load the XML File */
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(xmlFile);
+            doc.getDocumentElement().normalize();
+
+            /* get State Providers modules */
+            NodeList stateproviderNodes = doc.getElementsByTagName(TmfXmlStrings.STATE_PROVIDER);
+            for (int i = 0; i < stateproviderNodes.getLength(); i++) {
+                Element node = (Element) stateproviderNodes.item(i);
+
+                IAnalysisModuleHelper helper = new TmfAnalysisModuleHelperXml(xmlFile, node, XmlAnalysisModuleType.STATE_SYSTEM);
+                fModules.add(helper);
+            }
+        } catch (ParserConfigurationException | SAXException | IOException e) {
+            Activator.logError("Error opening XML file", e); //$NON-NLS-1$
+        }
+    }
+
+    private static void populateBuiltinModules() {
+        /* Get the XML files advertised through the extension point */
+        IConfigurationElement[] elements = Platform.getExtensionRegistry().getConfigurationElementsFor(TMF_XML_BUILTIN_ID);
+        for (IConfigurationElement element : elements) {
+            if (element.getName().equals(XML_FILE_ELEMENT)) {
+                String filename = element.getAttribute(XML_FILE_ATTRIB);
+                String name = element.getContributor().getName();
+                if (name != null) {
+                    Bundle bundle = Platform.getBundle(name);
+                    if (bundle != null) {
+                        try {
+                            URL xmlUrl = bundle.getResource(filename);
+                            URL locatedURL = FileLocator.toFileURL(xmlUrl);
+                            processFile(new File(locatedURL.getFile()));
+                        } catch (IOException e) {
+                            /* ignore */
+                        }
+                    }
+                }
+            }
+        }
+    }
+
     private static void populateAnalysisModules() {
-        fModules = new ArrayList<>();
         IPath pathToFiles = XmlUtils.getXmlFilesPath();
         File fFolder = pathToFiles.toFile();
         if (!(fFolder.isDirectory() && fFolder.exists())) {
             return;
         }
         for (File xmlFile : fFolder.listFiles()) {
-            if (!XmlUtils.xmlValidate(xmlFile).isOK()) {
-                continue;
-            }
-
-            try {
-                /* Load the XML File */
-                DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-                DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-                Document doc = dBuilder.parse(xmlFile);
-                doc.getDocumentElement().normalize();
-
-                /* get State Providers modules */
-                NodeList stateproviderNodes = doc.getElementsByTagName(TmfXmlStrings.STATE_PROVIDER);
-                for (int i = 0; i < stateproviderNodes.getLength(); i++) {
-                    Element node = (Element) stateproviderNodes.item(i);
-
-                    IAnalysisModuleHelper helper = new TmfAnalysisModuleHelperXml(xmlFile, node, XmlAnalysisModuleType.STATE_SYSTEM);
-                    fModules.add(helper);
-                }
-            } catch (ParserConfigurationException | SAXException | IOException e) {
-                Activator.logError("Error opening XML file", e); //$NON-NLS-1$
-            }
+            processFile(xmlFile);
         }
     }
 
