b36d77730f4877a356b65a4256d3d8075b14209a
Patrick Tasse <patrick.tasse@gmail.com>
2014-03-26 10:43:43 -0400

    tmf: Add Open With menu for trace elements in project explorer
    The menu item is only available when the trace resource is a file.
    
    Change-Id: I6951764fe5e357467020938c3f3802621e591aef
    Signed-off-by: Patrick Tasse <patrick.tasse@gmail.com>
    Reviewed-on: https://git.eclipse.org/r/23926
    Tested-by: Hudson CI
    Reviewed-by: Alexandre Montplaisir <alexmonthy@voxpopuli.im>
    IP-Clean: Alexandre Montplaisir <alexmonthy@voxpopuli.im>
    Tested-by: Alexandre Montplaisir <alexmonthy@voxpopuli.im>

--- org.eclipse.linuxtools.tmf.ui/src/org/eclipse/linuxtools/internal/tmf/ui/project/handlers/Messages.java
+++ org.eclipse.linuxtools.tmf.ui/src/org/eclipse/linuxtools/internal/tmf/ui/project/handlers/Messages.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2013 Ericsson
+ * Copyright (c) 2011, 2014 Ericsson
  *
  * All rights reserved. This program and the accompanying materials are
  * made available under the terms of the Eclipse Public License v1.0 which
@@ -51,6 +51,8 @@ public class Messages extends NLS {
     public static String SynchronizeTracesHandler_ErrorSynchingForTrace;
 
     public static String AnalysisModule_Help;
+
+    public static String TmfActionProvider_OpenWith;
 
     static {
         // initialize resource bundle
--- org.eclipse.linuxtools.tmf.ui/src/org/eclipse/linuxtools/internal/tmf/ui/project/handlers/TmfActionProvider.java
+++ org.eclipse.linuxtools.tmf.ui/src/org/eclipse/linuxtools/internal/tmf/ui/project/handlers/TmfActionProvider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright (c) 2012, 2013 Ericsson
+* Copyright (c) 2012, 2014 Ericsson
  *
  * All rights reserved. This program and the accompanying materials are
  * made available under the terms of the Eclipse Public License v1.0 which
@@ -12,11 +12,20 @@
 
 package org.eclipse.linuxtools.internal.tmf.ui.project.handlers;
 
+import org.eclipse.core.resources.IFile;
+import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.jface.action.MenuManager;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.linuxtools.tmf.ui.project.model.TmfTraceElement;
 import org.eclipse.ui.IActionBars;
+import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.actions.ActionFactory;
+import org.eclipse.ui.actions.OpenWithMenu;
 import org.eclipse.ui.navigator.CommonActionProvider;
 import org.eclipse.ui.navigator.ICommonActionConstants;
 import org.eclipse.ui.navigator.ICommonActionExtensionSite;
+import org.eclipse.ui.navigator.ICommonMenuConstants;
 import org.eclipse.ui.navigator.ICommonViewerSite;
 import org.eclipse.ui.navigator.ICommonViewerWorkbenchSite;
 
@@ -31,6 +40,8 @@ public class TmfActionProvider extends CommonActionProvider {
     private DeleteAction deleteAction;
     private RefreshAction refreshAction;
 
+    private IWorkbenchPage page;
+
     /**
      * Default constructor
      */
@@ -42,9 +53,26 @@ public class TmfActionProvider extends CommonActionProvider {
         ICommonViewerSite viewSite = aSite.getViewSite();
         if (viewSite instanceof ICommonViewerWorkbenchSite) {
             ICommonViewerWorkbenchSite workbenchSite = (ICommonViewerWorkbenchSite) viewSite;
-            openAction = new OpenAction(workbenchSite.getPage(), workbenchSite.getSelectionProvider());
-            deleteAction = new DeleteAction(workbenchSite.getPage(), workbenchSite.getSelectionProvider());
-            refreshAction = new RefreshAction(workbenchSite.getPage(), workbenchSite.getSelectionProvider());
+            page = workbenchSite.getPage();
+            openAction = new OpenAction(page, workbenchSite.getSelectionProvider());
+            deleteAction = new DeleteAction(page, workbenchSite.getSelectionProvider());
+            refreshAction = new RefreshAction(page, workbenchSite.getSelectionProvider());
+        }
+    }
+
+    @Override
+    public void fillContextMenu(IMenuManager menu) {
+        ISelection selection = getContext().getSelection();
+        if (selection instanceof IStructuredSelection) {
+            IStructuredSelection structuredSelection = (IStructuredSelection) selection;
+            if (structuredSelection.size() == 1 && structuredSelection.getFirstElement() instanceof TmfTraceElement) {
+                TmfTraceElement traceElement = (TmfTraceElement) structuredSelection.getFirstElement();
+                if (traceElement.getResource() instanceof IFile) {
+                    MenuManager openWithMenu = new MenuManager(Messages.TmfActionProvider_OpenWith);
+                    openWithMenu.add(new OpenWithMenu(page, traceElement.getResource()));
+                    menu.insertAfter(ICommonMenuConstants.GROUP_OPEN_WITH, openWithMenu);
+                }
+            }
         }
     }
 
