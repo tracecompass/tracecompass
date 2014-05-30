b44db33fc5bc4d3d9332677d42e596ef8a6a1b2f
Patrick Tasse <patrick.tasse@gmail.com>
2014-05-30 16:54:48 -0400

    tmf: Bug 436263: BufferedRandomAccessFile read does not return -1 on EOF
    Change-Id: I06b056af1249604ec1113035c623a7da3eb90cbd
    Signed-off-by: Patrick Tasse <patrick.tasse@gmail.com>
    Reviewed-on: https://git.eclipse.org/r/27624
    Tested-by: Hudson CI
    Reviewed-by: Alexandre Montplaisir <alexmonthy@voxpopuli.im>
    (cherry picked from commit acc60287f48004b0293305627040cdf31431b9fe)
    Reviewed-on: https://git.eclipse.org/r/27852

--- org.eclipse.linuxtools.tmf.core/src/org/eclipse/linuxtools/tmf/core/io/BufferedRandomAccessFile.java
+++ org.eclipse.linuxtools.tmf.core/src/org/eclipse/linuxtools/tmf/core/io/BufferedRandomAccessFile.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010, 2013 Ericsson
+ * Copyright (c) 2010, 2014 Ericsson
  *
  * All rights reserved. This program and the accompanying materials are
  * made available under the terms of the Eclipse Public License v1.0 which
@@ -140,6 +140,9 @@ public class BufferedRandomAccessFile extends RandomAccessFile {
             if (c != -1) {
                 b[off + i] = (byte) c;
             } else {
+                if (i == 0) {
+                    return -1;
+                }
                 return i;
             }
         }
