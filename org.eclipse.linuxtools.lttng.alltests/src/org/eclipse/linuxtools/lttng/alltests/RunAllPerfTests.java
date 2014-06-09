088b4f19605a38f3c5511002e933d73128944044
Geneviève Bastien <gbastien+lttng@versatic.net>
2014-06-09 15:33:28 -0400

    tmf: Introduce performance tests to TMF
    Add separate source directories in test packages specifically for
    performance tests based on org.eclipse.test.performance.
    
    Change-Id: Ia28d5269a6ba81f0727eb2a65858c97566dbf1d9
    Signed-off-by: Geneviève Bastien <gbastien+lttng@versatic.net>
    Signed-off-by: Alexandre Montplaisir <alexmonthy@voxpopuli.im>
    Reviewed-on: https://git.eclipse.org/r/28282
    Tested-by: Hudson CI

---
+++ org.eclipse.linuxtools.lttng.alltests/src/org/eclipse/linuxtools/lttng/alltests/RunAllPerfTests.java
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ * Copyright (c) 2014 École Polytechnique de Montréal
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Geneviève Bastien - Initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.linuxtools.lttng.alltests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Master test suite for all Linux Tools LTTng performance tests.
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+    org.eclipse.linuxtools.tmf.ctf.core.tests.perf.AllPerfTests.class
+})
+public class RunAllPerfTests {
+
+}
