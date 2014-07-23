b2b163e9680382040237bca02b03a08002a0acb0
Alexandre Montplaisir <alexmonthy@voxpopuli.im>
2014-07-23 13:17:36 -0400

    lttng: Add pcap.core to the Maven build
    And added a redundant annotation to to fix the lack-of-inheritance
    problem with Maven's compiler.
    
    Change-Id: Ib78f9dbbe1842da49a2509eb3e79913b1a94783d
    Signed-off-by: Alexandre Montplaisir <alexmonthy@voxpopuli.im>
    Reviewed-on: https://git.eclipse.org/r/30372
    Reviewed-by: Matthew Khouzam <matthew.khouzam@ericsson.com>
    Tested-by: Matthew Khouzam <matthew.khouzam@ericsson.com>

--- org.eclipse.linuxtools.pcap.core/src/org/eclipse/linuxtools/pcap/core/util/ConversionHelper.java
+++ org.eclipse.linuxtools.pcap.core/src/org/eclipse/linuxtools/pcap/core/util/ConversionHelper.java
@@ -16,6 +16,7 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.linuxtools.pcap.core.protocol.ethernet2.EthernetIIValues;
 import org.eclipse.linuxtools.pcap.core.protocol.ipv4.IPv4Values;
 
@@ -26,9 +27,8 @@ import org.eclipse.linuxtools.pcap.core.protocol.ipv4.IPv4Values;
  */
 public final class ConversionHelper {
 
-
     @SuppressWarnings("null")
-    private static final char[] HEX_ARRAY = "0123456789abcdef".toCharArray(); //$NON-NLS-1$
+    private static final @NonNull char[] HEX_ARRAY = "0123456789abcdef".toCharArray(); //$NON-NLS-1$
     private static final String EMPTY_STRING = ""; //$NON-NLS-1$
     private static final String DEFAULT_TIME_PATTERN = "yyyy-MM-dd HH:mm:ss.SSS"; //$NON-NLS-1$
     private static final DateFormat DATE_FORMATTER = new SimpleDateFormat(DEFAULT_TIME_PATTERN);
