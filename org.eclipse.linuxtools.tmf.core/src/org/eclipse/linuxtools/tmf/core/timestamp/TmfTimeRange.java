5f490d2f821549cfcec29c520a797af5431fc6a0
Vincent Perot <vincent.perot@ericsson.com>
2014-07-30 14:49:41 -0400

    tmf: make TmfTimeRange static ranges NonNull
    This avoids useless null checks in tmf.pcap
    
    Change-Id: Ice73d87d4119469f297d434ca6aca10233807160
    Signed-off-by: Vincent Perot <vincent.perot@ericsson.com>
    Reviewed-on: https://git.eclipse.org/r/30758
    Reviewed-by: Matthew Khouzam <matthew.khouzam@ericsson.com>
    Reviewed-by: Alexandre Montplaisir <alexmonthy@voxpopuli.im>
    Tested-by: Hudson CI

--- org.eclipse.linuxtools.tmf.core/src/org/eclipse/linuxtools/tmf/core/timestamp/TmfTimeRange.java
+++ org.eclipse.linuxtools.tmf.core/src/org/eclipse/linuxtools/tmf/core/timestamp/TmfTimeRange.java
@@ -13,6 +13,8 @@
 
 package org.eclipse.linuxtools.tmf.core.timestamp;
 
+import org.eclipse.jdt.annotation.NonNull;
+
 /**
  * A utility class to define and manage time ranges.
  *
@@ -31,12 +33,12 @@ public class TmfTimeRange {
     /**
      * The full possible time range
      */
-    public static final TmfTimeRange ETERNITY = new EternityTimeRange();
+    public static final @NonNull TmfTimeRange ETERNITY = new EternityTimeRange();
 
     /**
      * The null time range
      */
-    public static final TmfTimeRange NULL_RANGE = new TmfTimeRange();
+    public static final @NonNull TmfTimeRange NULL_RANGE = new TmfTimeRange();
 
     // ------------------------------------------------------------------------
     // Attributes
