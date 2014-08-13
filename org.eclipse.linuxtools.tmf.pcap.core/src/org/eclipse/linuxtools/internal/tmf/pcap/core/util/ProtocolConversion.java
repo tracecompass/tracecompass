87e8cb47b094c4be1a2d2bd82280ae1e4989cda2
Matthew Khouzam <matthew.khouzam@ericsson.com>
2014-08-13 17:20:34 -0400

    pcap: Fix null warning in ProtocolConversion
    Rework the ProtocolConversion utility class to switch/case
    through the existing values, instead of relying on
    non-null-annotated methods.
    
    Also updated the documentation.
    
    Change-Id: I9de733029de08e976acca313a8ad4601d3db0a8b
    Signed-off-by: Matthew Khouzam <matthew.khouzam@ericsson.com>
    Signed-off-by: Vincent Perot <vincent.perot@ericsson.com>
    Signed-off-by: Alexandre Montplaisir <alexmonthy@voxpopuli.im>
    Reviewed-on: https://git.eclipse.org/r/31588
    Tested-by: Hudson CI

--- org.eclipse.linuxtools.tmf.pcap.core/src/org/eclipse/linuxtools/internal/tmf/pcap/core/util/ProtocolConversion.java
+++ org.eclipse.linuxtools.tmf.pcap.core/src/org/eclipse/linuxtools/internal/tmf/pcap/core/util/ProtocolConversion.java
@@ -12,7 +12,6 @@
 
 package org.eclipse.linuxtools.internal.tmf.pcap.core.util;
 
-import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.linuxtools.pcap.core.protocol.Protocol;
 import org.eclipse.linuxtools.tmf.pcap.core.protocol.TmfProtocol;
 
@@ -22,34 +21,60 @@ import org.eclipse.linuxtools.tmf.pcap.core.protocol.TmfProtocol;
  *
  * @author Vincent Perot
  */
-public class ProtocolConversion {
+public final class ProtocolConversion {
+
+    private ProtocolConversion() {}
 
     /**
-     * Wrap a Protocol into a TmfProtocol.
+     * Wrap a {@link Protocol} into a {@link TmfProtocol}.
      *
      * @param protocol
-     *            The Protocol.
-     * @return The TmfProtocol.
+     *            The {@link Protocol} to match
+     * @return The TmfProtocol
      */
     public static TmfProtocol wrap(Protocol protocol) {
-        @SuppressWarnings("null")
-        @NonNull String name = protocol.name();
-
-        @NonNull TmfProtocol wrappedProtocol = TmfProtocol.valueOf(name);
-        return wrappedProtocol;
+        switch (protocol) {
+        case ETHERNET_II:
+            return TmfProtocol.ETHERNET_II;
+        case IPV4:
+            return TmfProtocol.IPV4;
+        case PCAP:
+            return TmfProtocol.PCAP;
+        case TCP:
+            return TmfProtocol.TCP;
+        case UDP:
+            return TmfProtocol.UDP;
+        case UNKNOWN:
+            return TmfProtocol.UNKNOWN;
+        default:
+            throw new IllegalArgumentException();
+        }
     }
 
     /**
-     * Unwrap a TmfProtocol into a Protocol.
+     * Unwrap a {@link TmfProtocol} from a {@link Protocol}.
      *
      * @param protocol
-     *            The TmfProtocol.
-     * @return The Protocol.
+     *            The TmfProtocol
+     * @return The Protocol
      */
     public static Protocol unwrap(TmfProtocol protocol) {
-        @SuppressWarnings("null")
-        @NonNull String name = protocol.name();
-        return Protocol.valueOf(name);
+        switch (protocol) {
+        case ETHERNET_II:
+            return Protocol.ETHERNET_II;
+        case IPV4:
+            return Protocol.IPV4;
+        case PCAP:
+            return Protocol.PCAP;
+        case TCP:
+            return Protocol.TCP;
+        case UDP:
+            return Protocol.UDP;
+        case UNKNOWN:
+            return Protocol.UNKNOWN;
+        default:
+            throw new IllegalArgumentException();
+        }
     }
 
 }
