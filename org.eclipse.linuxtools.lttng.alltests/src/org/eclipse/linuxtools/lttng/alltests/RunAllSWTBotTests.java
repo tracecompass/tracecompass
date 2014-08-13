6e2574ffe73429842443af9a10688a0001f86288
Matthew Khouzam <matthew.khouzam@ericsson.com>
2014-08-12 23:21:51 -0400

    tmf: enable swtbot pcap tests
    Change-Id: I21ee396f3edd2e24a6610751ee47df982acdfacc
    Signed-off-by: Matthew Khouzam <matthew.khouzam@ericsson.com>
    Signed-off-by: Vincent Perot <vincent.perot@ericsson.com>
    Reviewed-on: https://git.eclipse.org/r/31492
    Reviewed-by: Bernd Hufmann <bernd.hufmann@ericsson.com>
    Tested-by: Bernd Hufmann <bernd.hufmann@ericsson.com>
    Tested-by: Hudson CI

--- org.eclipse.linuxtools.lttng.alltests/src/org/eclipse/linuxtools/lttng/alltests/RunAllSWTBotTests.java
+++ org.eclipse.linuxtools.lttng.alltests/src/org/eclipse/linuxtools/lttng/alltests/RunAllSWTBotTests.java
@@ -25,6 +25,7 @@ import org.junit.runners.Suite;
 @Suite.SuiteClasses({
     org.eclipse.linuxtools.tmf.ui.swtbot.tests.AllTmfUISWTBotTests.class,
     org.eclipse.linuxtools.tmf.ctf.ui.swtbot.tests.AllTests.class,
+    org.eclipse.linuxtools.tmf.pcap.ui.swtbot.tests.AllTests.class,
     org.eclipse.linuxtools.lttng2.kernel.ui.swtbot.tests.AllTests.class
 })
 public class RunAllSWTBotTests {
