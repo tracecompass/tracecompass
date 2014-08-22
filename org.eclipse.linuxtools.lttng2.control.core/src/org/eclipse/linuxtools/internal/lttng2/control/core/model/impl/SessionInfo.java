801699425963012c7b22d3c15e7b04072041b912
Jonathan Rajotte Julien <jonathan.rajotte-julien@ericsson.com>
2014-08-22 17:10:42 -0400

    tmf: lttngControl: Fix remote import on remote snapshot session
    Change-Id: Ic1e6f8724cd852a3b371f4eac3d8fd5a64f2f01a
    Signed-off-by: Jonathan Rajotte Julien <jonathan.rajotte-julien@ericsson.com>
    Reviewed-on: https://git.eclipse.org/r/32183
    Reviewed-by: Bernd Hufmann <bernd.hufmann@ericsson.com>
    Tested-by: Bernd Hufmann <bernd.hufmann@ericsson.com>
    Tested-by: Hudson CI
    Reviewed-by: Matthew Khouzam <matthew.khouzam@ericsson.com>

--- org.eclipse.linuxtools.lttng2.control.core/src/org/eclipse/linuxtools/internal/lttng2/control/core/model/impl/SessionInfo.java
+++ org.eclipse.linuxtools.lttng2.control.core/src/org/eclipse/linuxtools/internal/lttng2/control/core/model/impl/SessionInfo.java
@@ -185,6 +185,9 @@ public class SessionInfo extends TraceInfo implements ISessionInfo {
 
     @Override
     public boolean isStreamedTrace() {
+        if (isSnapshotSession() && getSnapshotInfo() != null) {
+            return getSnapshotInfo().isStreamedSnapshot();
+        }
         return fIsStreamedTrace;
     }
 
