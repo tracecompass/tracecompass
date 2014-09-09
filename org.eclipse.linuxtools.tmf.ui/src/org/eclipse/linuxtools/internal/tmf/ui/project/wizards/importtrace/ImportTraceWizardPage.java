edaf6a5ee25c366c98fb14cc5faab4a505747dae
Marc-Andre Laperle <marc-andre.laperle@ericsson.com>
2014-09-09 17:49:35 -0400

    tmf: Fix IllegalArgumentException when importing from the root directory
    Bug: 443651
    Change-Id: I5fb050a4c35f27c4393f30ba0b9836cc656e76de
    Signed-off-by: Marc-Andre Laperle <marc-andre.laperle@ericsson.com>
    Reviewed-on: https://git.eclipse.org/r/33152
    Tested-by: Hudson CI
    Reviewed-by: Bernd Hufmann <bernd.hufmann@ericsson.com>
    Tested-by: Bernd Hufmann <bernd.hufmann@ericsson.com>
    (cherry picked from commit 47d1ed9b786018f2f5da5c86a1fa2d4386fd8c9f)
    Reviewed-on: https://git.eclipse.org/r/33154

--- org.eclipse.linuxtools.tmf.ui/src/org/eclipse/linuxtools/internal/tmf/ui/project/wizards/importtrace/ImportTraceWizardPage.java
+++ org.eclipse.linuxtools.tmf.ui/src/org/eclipse/linuxtools/internal/tmf/ui/project/wizards/importtrace/ImportTraceWizardPage.java
@@ -757,9 +757,13 @@ public class ImportTraceWizardPage extends WizardResourceImportPage {
             return adapted;
         }
 
-       public IFileSystemObject getIFileSystemObject(Object o) {
+        public IFileSystemObject getIFileSystemObject(Object o) {
+            if (o == null) {
+                return null;
+            }
+
             if (o instanceof File) {
-                return new FileFileSystemObject((File)o);
+                return new FileFileSystemObject((File) o);
             } else if (o instanceof TarEntry) {
                 return new TarFileSystemObject((TarEntry) o, fArchivePath);
             } else if (o instanceof ZipEntry) {
