7ce905590f1cbb29aa9717383c03840de754ae1f
Alexandre Montplaisir <alexmonthy@voxpopuli.im>
2014-09-26 16:11:32 -0400

    btf: Rename packages to org.eclipse.tracecompass.*
    Change-Id: I56194cc3c13c57c315aedd300efaf49bf0e57058
    Signed-off-by: Alexandre Montplaisir <alexmonthy@voxpopuli.im>

--- org.eclipse.tracecompass.alltests/src/org/eclipse/linuxtools/lttng/alltests/RunAllCoreTests.java
+++ org.eclipse.tracecompass.alltests/src/org/eclipse/linuxtools/lttng/alltests/RunAllCoreTests.java
@@ -20,7 +20,7 @@ import org.junit.runners.Suite;
  */
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-    org.eclipse.linuxtools.btf.core.tests.AllTests.class,
+    org.eclipse.tracecompass.btf.core.tests.AllTests.class,
     org.eclipse.tracecompass.ctf.core.tests.AllCtfCoreTests.class,
     org.eclipse.tracecompass.ctf.parser.tests.AllCtfParserTests.class,
     org.eclipse.tracecompass.gdbtrace.core.tests.AllGdbTraceCoreTests.class,
---
+++ org.eclipse.tracecompass.btf.ui/src/org/eclipse/tracecompass/btf/ui/BtfEventTableColumns.java
@@ -0,0 +1,140 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Ericsson
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Matthew Khouzam - Initial API and implementation
+ *   Alexandre Montplaisir - Update to new Event Table API
+ *******************************************************************************/
+
+package org.eclipse.tracecompass.btf.ui;
+
+import java.util.Collection;
+
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.tracecompass.btf.core.trace.BtfColumnNames;
+import org.eclipse.tracecompass.tmf.core.event.ITmfEvent;
+import org.eclipse.tracecompass.tmf.ui.viewers.events.columns.ITmfEventTableColumns;
+import org.eclipse.tracecompass.tmf.ui.viewers.events.columns.TmfEventTableColumn;
+import org.eclipse.tracecompass.tmf.ui.viewers.events.columns.TmfEventTableFieldColumn;
+
+import com.google.common.collect.ImmutableList;
+
+/**
+ * Columns to use in the BTF event table
+ *
+ * @author Alexandre Montplaisir
+ */
+public class BtfEventTableColumns implements ITmfEventTableColumns {
+
+    // ------------------------------------------------------------------------
+    // Column definition
+    // ------------------------------------------------------------------------
+
+    @SuppressWarnings("null")
+    private static final @NonNull Collection<TmfEventTableColumn> BTF_COLUMNS = ImmutableList.of(
+            TmfEventTableColumn.BaseColumns.TIMESTAMP,
+            new BtfSourceColumn(),
+            new BtfSourceInstanceColumn(),
+            TmfEventTableColumn.BaseColumns.EVENT_TYPE,
+            new BtfTargetColumn(),
+            new BtfTargetInstanceColumn(),
+            new BtfEventColumn(),
+            new BtfNotesColumn()
+            );
+
+    /**
+     * The "source" column, whose value comes from {@link ITmfEvent#getSource()}
+     */
+    private static class BtfSourceColumn extends TmfEventTableColumn {
+
+        public BtfSourceColumn() {
+            super(BtfColumnNames.SOURCE.toString(), null);
+        }
+
+        @Override
+        public String getItemString(ITmfEvent event) {
+            String ret = event.getSource();
+            return (ret == null ? EMPTY_STRING : ret);
+        }
+
+        @Override
+        public String getFilterFieldId() {
+            return ITmfEvent.EVENT_FIELD_SOURCE;
+        }
+    }
+
+    /**
+     * The "source instance" column, whose value comes from the field of the
+     * same name.
+     */
+    private static class BtfSourceInstanceColumn extends TmfEventTableFieldColumn {
+        public BtfSourceInstanceColumn() {
+            super(BtfColumnNames.SOURCE_INSTANCE.toString());
+        }
+    }
+
+    /**
+     * The "target" column, taking its value from
+     * {@link ITmfEvent#getReference()}.
+     */
+    private static class BtfTargetColumn extends TmfEventTableColumn {
+
+        public BtfTargetColumn() {
+            super(BtfColumnNames.TARGET.toString());
+        }
+
+        @Override
+        public String getItemString(ITmfEvent event) {
+            String ret = event.getReference();
+            return (ret == null ? EMPTY_STRING : ret);
+        }
+
+        @Override
+        public String getFilterFieldId() {
+            return ITmfEvent.EVENT_FIELD_REFERENCE;
+        }
+    }
+
+    /**
+     * The "target instance" column, whose value comes from the field of the
+     * same name.
+     */
+    private static class BtfTargetInstanceColumn extends TmfEventTableFieldColumn {
+        public BtfTargetInstanceColumn() {
+            super(BtfColumnNames.TARGET_INSTANCE.toString());
+        }
+    }
+
+    /**
+     * The "event" column, whose value comes from the field of the same name.
+     */
+    private static class BtfEventColumn extends TmfEventTableFieldColumn {
+        public BtfEventColumn() {
+            super(BtfColumnNames.EVENT.toString());
+        }
+    }
+
+    /**
+     * The "notes" column, whose value comes from the field of the same name, if
+     * present.
+     */
+    private static class BtfNotesColumn extends TmfEventTableFieldColumn {
+        public BtfNotesColumn() {
+            super(BtfColumnNames.NOTES.toString());
+        }
+    }
+
+    // ------------------------------------------------------------------------
+    // ITmfEventTableColumns
+    // ------------------------------------------------------------------------
+
+    @Override
+    public Collection<? extends TmfEventTableColumn> getEventTableColumns() {
+        return BTF_COLUMNS;
+    }
+}
