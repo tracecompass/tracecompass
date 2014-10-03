35a15531a032c0bca2cc956bc97a319e5496fb66
Patrick Tasse <patrick.tasse@gmail.com>
2014-10-03 16:23:43 -0400

    tmf: Fix copy trace handler enabled in unrelated dialogs
    Ctrl-C no longer opens the Copy Trace dialog from another dialog when
    the current selection in the Project Explorer is a trace element.
    
    The same fix is applied to the copy experiment handler.
    
    Change-Id: I4eb9df578d2227b74312d7fe660b2a3caf2b2627
    Signed-off-by: Patrick Tasse <patrick.tasse@gmail.com>
    Reviewed-on: https://git.eclipse.org/r/34415
    Tested-by: Hudson CI
    Reviewed-by: Bernd Hufmann <bernd.hufmann@ericsson.com>
    Tested-by: Bernd Hufmann <bernd.hufmann@ericsson.com>

--- org.eclipse.linuxtools.tmf.ui/src/org/eclipse/linuxtools/internal/tmf/ui/project/handlers/CopyExperimentHandler.java
+++ org.eclipse.linuxtools.tmf.ui/src/org/eclipse/linuxtools/internal/tmf/ui/project/handlers/CopyExperimentHandler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2013 Ericsson
+ * Copyright (c) 2009, 2014 Ericsson
  *
  * All rights reserved. This program and the accompanying materials are
  * made available under the terms of the Eclipse Public License v1.0 which
@@ -8,6 +8,7 @@
  *
  * Contributors:
  *   Francois Chouinard - Initial API and implementation
+ *   Patrick Tasse - Remove enable check
  *******************************************************************************/
 
 package org.eclipse.linuxtools.internal.tmf.ui.project.handlers;
@@ -16,63 +17,17 @@ import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.ISelectionProvider;
-import org.eclipse.jface.viewers.TreeSelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.linuxtools.tmf.ui.project.model.TmfExperimentElement;
 import org.eclipse.linuxtools.tmf.ui.project.wizards.CopyExperimentDialog;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.ui.IWorkbenchPage;
-import org.eclipse.ui.IWorkbenchPart;
-import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.handlers.HandlerUtil;
 
 /**
  * <b><u>CopyExperimentHandler</u></b>
  * <p>
- * TODO: Implement me. Please.
  */
 public class CopyExperimentHandler extends AbstractHandler {
-
-    private TmfExperimentElement fExperiment = null;
-
-    // ------------------------------------------------------------------------
-    // isEnabled
-    // ------------------------------------------------------------------------
-
-    @Override
-    public boolean isEnabled() {
-
-        // Check if we are closing down
-        IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
-        if (window == null) {
-            return false;
-        }
-
-        // Get the selection
-        IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
-        IWorkbenchPart part = page.getActivePart();
-        if (part == null) {
-            return false;
-        }
-        ISelectionProvider selectionProvider = part.getSite().getSelectionProvider();
-        if (selectionProvider == null) {
-            return false;
-        }
-        ISelection selection = selectionProvider.getSelection();
-
-        // Make sure there is only selection and that it is an experiment
-        fExperiment = null;
-        if (selection instanceof TreeSelection) {
-            TreeSelection sel = (TreeSelection) selection;
-            // There should be only one item selected as per the plugin.xml
-            Object element = sel.getFirstElement();
-            if (element instanceof TmfExperimentElement) {
-                fExperiment = (TmfExperimentElement) element;
-            }
-        }
-
-        return (fExperiment != null);
-    }
 
     // ------------------------------------------------------------------------
     // Execution
@@ -81,15 +36,16 @@ public class CopyExperimentHandler extends AbstractHandler {
     @Override
     public Object execute(ExecutionEvent event) throws ExecutionException {
 
-        // Check if we are closing down
-        IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
-        if (window == null) {
+        // Get selection already validated by handler in plugin.xml
+        ISelection selection = HandlerUtil.getCurrentSelectionChecked(event);
+        if (!(selection instanceof IStructuredSelection)) {
             return null;
         }
+        TmfExperimentElement experiment = (TmfExperimentElement) ((IStructuredSelection) selection).getFirstElement();
 
         // Fire the Copy Experiment dialog
-        Shell shell = window.getShell();
-        CopyExperimentDialog dialog = new CopyExperimentDialog(shell, fExperiment);
+        Shell shell = HandlerUtil.getActiveShellChecked(event);
+        CopyExperimentDialog dialog = new CopyExperimentDialog(shell, experiment);
         dialog.open();
 
         return null;
