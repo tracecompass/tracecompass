e110fed15992a1251cc9f03104a70f73eda743d3
Alexandre Montplaisir <alexmonthy@voxpopuli.im>
2014-12-03 19:07:13 -0500

    common: Add a new "common" plugin for shared functionality
    More and more we're seeing functionality and utils classes duplicated
    across plugins. It's making sense to have a central, common plugin that
    everybody else can depend on.
    
    Change-Id: I8d98518acb81c43766d1782432180a1eb7a5e12f
    Signed-off-by: Alexandre Montplaisir <alexmonthy@voxpopuli.im>
    Reviewed-on: https://git.eclipse.org/r/37617
    Reviewed-by: Hudson CI
    Reviewed-by: Matthew Khouzam <matthew.khouzam@ericsson.com>

---
+++ org.eclipse.tracecompass.common.core/src/org/eclipse/tracecompass/common/core/TraceCompassActivator.java
@@ -0,0 +1,197 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Ericsson
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Alexandre Montplaisir - Initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.tracecompass.common.core;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Plugin;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.jdt.annotation.Nullable;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ *
+ * @author Alexandre Montplaisir
+ */
+public abstract class TraceCompassActivator extends Plugin {
+
+    // ------------------------------------------------------------------------
+    // Attributes
+    // ------------------------------------------------------------------------
+
+    /** Map of all the registered activators, indexed by plugin ID */
+    private static final Map<String, TraceCompassActivator> ACTIVATORS =
+            new HashMap<>();
+
+    /** This instance's plug-in ID */
+    private final String fPluginId;
+
+    // ------------------------------------------------------------------------
+    // Constructors
+    // ------------------------------------------------------------------------
+
+    /**
+     * The constructor
+     *
+     * @param pluginID
+     *            The ID of the plugin
+     */
+    public TraceCompassActivator(String pluginID) {
+        fPluginId = pluginID;
+    }
+
+    // ------------------------------------------------------------------------
+    // Accessors
+    // ------------------------------------------------------------------------
+
+    /**
+     * Return this plug-in's ID.
+     *
+     * @return The plug-in ID
+     */
+    public String getPluginId() {
+        return fPluginId;
+    }
+
+    /**
+     * Get a registered activator. Subclasses should implement their own public
+     * getInstance() method, which returns the result of this.
+     *
+     * @param id
+     *            The activator's plugin ID
+     * @return The corresponding activator
+     */
+    protected static TraceCompassActivator getInstance(String id) {
+        TraceCompassActivator ret = ACTIVATORS.get(id);
+        if (ret == null) {
+            /* The activator should be registered at this point! */
+            throw new IllegalStateException();
+        }
+        return ret;
+    }
+
+    // ------------------------------------------------------------------------
+    // Abstract methods
+    // ------------------------------------------------------------------------
+
+    /**
+     * Additional actions to run at the plug-in startup
+     */
+    protected abstract void startActions();
+
+    /**
+     * Additional actions to run at the plug-in shtudown
+     */
+    protected abstract void stopActions();
+
+    // ------------------------------------------------------------------------
+    // ore.eclipse.core.runtime.Plugin
+    // ------------------------------------------------------------------------
+
+    @Override
+    public final void start(@Nullable BundleContext context) throws Exception {
+        super.start(context);
+        String id = this.getPluginId();
+        if (ACTIVATORS.containsKey(id)) {
+            logError("Duplicate Activator ID : " + id); //$NON-NLS-1$
+        }
+        ACTIVATORS.put(id, this);
+        startActions();
+    }
+
+    @Override
+    public final void stop(@Nullable BundleContext context) throws Exception {
+        stopActions();
+        ACTIVATORS.remove(this.getPluginId());
+        super.stop(context);
+    }
+
+    // ------------------------------------------------------------------------
+    // Logging helpers
+    // ------------------------------------------------------------------------
+
+    /**
+     * Logs a message with severity INFO in the runtime log of the plug-in.
+     *
+     * @param message
+     *            A message to log
+     */
+    public void logInfo(String message) {
+        getLog().log(new Status(IStatus.INFO, fPluginId, message));
+    }
+
+    /**
+     * Logs a message and exception with severity INFO in the runtime log of the
+     * plug-in.
+     *
+     * @param message
+     *            A message to log
+     * @param exception
+     *            A exception to log
+     */
+    public void logInfo(String message, Throwable exception) {
+        getLog().log(new Status(IStatus.INFO, fPluginId, message, exception));
+    }
+
+    /**
+     * Logs a message and exception with severity WARNING in the runtime log of
+     * the plug-in.
+     *
+     * @param message
+     *            A message to log
+     */
+    public void logWarning(String message) {
+        getLog().log(new Status(IStatus.WARNING, fPluginId, message));
+    }
+
+    /**
+     * Logs a message and exception with severity WARNING in the runtime log of
+     * the plug-in.
+     *
+     * @param message
+     *            A message to log
+     * @param exception
+     *            A exception to log
+     */
+    public void logWarning(String message, Throwable exception) {
+        getLog().log(new Status(IStatus.WARNING, fPluginId, message, exception));
+    }
+
+    /**
+     * Logs a message and exception with severity ERROR in the runtime log of
+     * the plug-in.
+     *
+     * @param message
+     *            A message to log
+     */
+    public void logError(String message) {
+        getLog().log(new Status(IStatus.ERROR, fPluginId, message));
+    }
+
+    /**
+     * Logs a message and exception with severity ERROR in the runtime log of
+     * the plug-in.
+     *
+     * @param message
+     *            A message to log
+     * @param exception
+     *            A exception to log
+     */
+    public void logError(String message, Throwable exception) {
+        getLog().log(new Status(IStatus.ERROR, fPluginId, message, exception));
+    }
+
+}
---
+++ org.eclipse.tracecompass.common.core/src/org/eclipse/tracecompass/internal/common/core/Activator.java
@@ -0,0 +1,48 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Ericsson
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Alexandre Montplaisir - Initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.tracecompass.internal.common.core;
+
+import org.eclipse.tracecompass.common.core.TraceCompassActivator;
+
+/**
+ * Plugin activator
+ */
+public class Activator extends TraceCompassActivator {
+
+    private static final String PLUGIN_ID = "org.eclipse.tracecompass.common.core"; //$NON-NLS-1$
+
+    /**
+     * Return the singleton instance of this activator.
+     *
+     * @return The singleton instance
+     */
+    public static Activator instance() {
+        return (Activator) TraceCompassActivator.getInstance(PLUGIN_ID);
+    }
+
+    /**
+     * Constructor
+     */
+    public Activator() {
+        super(PLUGIN_ID);
+    }
+
+    @Override
+    protected void startActions() {
+    }
+
+    @Override
+    protected void stopActions() {
+    }
+
+}
