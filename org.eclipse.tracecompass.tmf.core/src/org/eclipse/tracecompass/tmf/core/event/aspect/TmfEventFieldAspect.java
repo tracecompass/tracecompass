11124ee6204a151d42fcbb91f8a331e02ae65105
Patrick Tasse <patrick.tasse@gmail.com>
2014-12-17 17:45:09 -0500

    tmf: Support subfield array in TmfEventFieldAspect
    Change-Id: I3ada68c24d73b55a55851100a8269cd506fd56e5
    Signed-off-by: Patrick Tasse <patrick.tasse@gmail.com>
    Reviewed-on: https://git.eclipse.org/r/38512
    Reviewed-by: Bernd Hufmann <bernd.hufmann@ericsson.com>
    Tested-by: Bernd Hufmann <bernd.hufmann@ericsson.com>

--- org.eclipse.tracecompass.tmf.core/src/org/eclipse/tracecompass/tmf/core/event/aspect/TmfEventFieldAspect.java
+++ org.eclipse.tracecompass.tmf.core/src/org/eclipse/tracecompass/tmf/core/event/aspect/TmfEventFieldAspect.java
@@ -8,9 +8,13 @@
  *
  * Contributors:
  *   Alexandre Montplaisir - Initial API and implementation
+ *   Patrick Tasse - Support subfield array
  *******************************************************************************/
 
 package org.eclipse.tracecompass.tmf.core.event.aspect;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.eclipse.jdt.annotation.Nullable;
 import org.eclipse.tracecompass.tmf.core.event.ITmfEvent;
@@ -23,8 +27,12 @@ import org.eclipse.tracecompass.tmf.core.event.ITmfEventField;
  */
 public class TmfEventFieldAspect implements ITmfEventAspect {
 
+    private static final char SLASH = '/';
+    private static final char BACKSLASH = '\\';
+
     private final String fAspectName;
     private final String fFieldName;
+    private final @Nullable String[] fFieldArray;
 
     /**
      * Constructor
@@ -38,6 +46,12 @@ public class TmfEventFieldAspect implements ITmfEventAspect {
     public TmfEventFieldAspect(String aspectName, String fieldName) {
         fAspectName = aspectName;
         fFieldName = fieldName;
+        if (!fieldName.isEmpty() && fieldName.charAt(0) == SLASH) {
+            fFieldArray = getFieldArray(fieldName);
+        } else {
+            fFieldArray = null;
+        }
+
     }
 
     @Override
@@ -52,7 +66,12 @@ public class TmfEventFieldAspect implements ITmfEventAspect {
 
     @Override
     public @Nullable String resolve(ITmfEvent event) {
-        ITmfEventField field = event.getContent().getField(fFieldName);
+        ITmfEventField field;
+        if (fFieldArray == null) {
+            field = event.getContent().getField(fFieldName);
+        } else {
+            field = event.getContent().getSubField(fFieldArray);
+        }
         if (field == null) {
             return null;
         }
@@ -78,7 +97,10 @@ public class TmfEventFieldAspect implements ITmfEventAspect {
         if (this == obj) {
             return true;
         }
-        if (!(obj instanceof TmfEventFieldAspect)) {
+        if (obj == null) {
+            return false;
+        }
+        if (!this.getClass().equals(obj.getClass())) {
             return false;
         }
         TmfEventFieldAspect other = (TmfEventFieldAspect) obj;
@@ -89,5 +111,38 @@ public class TmfEventFieldAspect implements ITmfEventAspect {
             return false;
         }
         return true;
+    }
+
+    private static String[] getFieldArray(String field) {
+
+        StringBuilder sb = new StringBuilder();
+        List<String> list = new ArrayList<>();
+
+        // We start at 1 since the first character is a slash that we want to
+        // ignore.
+        for (int i = 1; i < field.length(); i++) {
+            char charAt = field.charAt(i);
+            if (charAt == SLASH) {
+                // char is slash. Cut here.
+                list.add(sb.toString());
+                sb = new StringBuilder();
+            } else if ((charAt == BACKSLASH) && (i < field.length() - 1) && (field.charAt(i + 1) == SLASH)) {
+                // Uninterpreted slash. Add it.
+                sb.append(SLASH);
+                i++;
+            } else {
+                // Any other character. Add.
+                sb.append(charAt);
+            }
+        }
+
+        // Last block. Add it to list.
+        list.add(sb.toString());
+
+        // Transform to array
+        String[] array = new String[list.size()];
+        list.toArray(array);
+
+        return array;
     }
 }
