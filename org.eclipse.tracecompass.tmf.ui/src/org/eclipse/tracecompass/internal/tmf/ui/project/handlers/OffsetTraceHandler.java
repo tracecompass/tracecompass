664030c5711533e1f10997dc4d6466d315c23812
Patrick Tasse <patrick.tasse@gmail.com>
2015-01-05 17:44:37 -0500

    tmf: Fix IndexOutOfBoundsException in OffsetDialog
    Exception occurred when opening the dialog from an empty experiment or
    trace folder.
    
    Change-Id: I454f76f24b3af3602fd2c52111ad63b1aae190e0
    Signed-off-by: Patrick Tasse <patrick.tasse@gmail.com>
    Reviewed-on: https://git.eclipse.org/r/39008
    Reviewed-by: Hudson CI
    Reviewed-by: Matthew Khouzam <matthew.khouzam@ericsson.com>
    Tested-by: Matthew Khouzam <matthew.khouzam@ericsson.com>

--- org.eclipse.tracecompass.tmf.ui/src/org/eclipse/tracecompass/internal/tmf/ui/project/handlers/OffsetTraceHandler.java
+++ org.eclipse.tracecompass.tmf.ui/src/org/eclipse/tracecompass/internal/tmf/ui/project/handlers/OffsetTraceHandler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2014 Ericsson
+ * Copyright (c) 2014, 2015 Ericsson
  *
  * All rights reserved. This program and the accompanying materials are
  * made available under the terms of the Eclipse Public License v1.0 which
@@ -77,6 +77,10 @@ public class OffsetTraceHandler extends AbstractHandler {
                     traceElements.addAll(folder.getTraces());
                 }
             }
+        }
+
+        if (traceElements.isEmpty()) {
+            return null;
         }
 
         final Map<TmfTraceElement, Long> offsets = new LinkedHashMap<>(traceElements.size());
