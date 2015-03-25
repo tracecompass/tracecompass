2e7668dd957861a2d59f1dfe137d0e0599e2f01f
Matthew Khouzam <matthew.khouzam@ericsson.com>
2015-03-25 17:47:16 -0400

    lttng: Add UST swtbot plug-in
    Change-Id: I5a6db70e104734c8158a57b3c635bdbbbf4837dd
    Signed-off-by: Matthew Khouzam <matthew.khouzam@ericsson.com>
    Reviewed-on: https://git.eclipse.org/r/44641
    Reviewed-by: Hudson CI
    Reviewed-by: Marc-Andre Laperle <marc-andre.laperle@ericsson.com>
    Tested-by: Marc-Andre Laperle <marc-andre.laperle@ericsson.com>
    Reviewed-on: https://git.eclipse.org/r/60816
    Reviewed-by: Patrick Tasse <patrick.tasse@gmail.com>
    Tested-by: Patrick Tasse <patrick.tasse@gmail.com>

--- releng/org.eclipse.tracecompass.alltests/src/org/eclipse/tracecompass/alltests/swtbot/RunAllSWTBotTests.java
+++ releng/org.eclipse.tracecompass.alltests/src/org/eclipse/tracecompass/alltests/swtbot/RunAllSWTBotTests.java
@@ -27,7 +27,8 @@ import org.junit.runners.Suite;
     org.eclipse.tracecompass.tmf.ctf.ui.swtbot.tests.AllTests.class,
     org.eclipse.tracecompass.tmf.pcap.ui.swtbot.tests.AllTests.class,
     org.eclipse.tracecompass.tmf.remote.ui.swtbot.tests.AllTmfRemoteUISWTBotTests.class,
-    org.eclipse.tracecompass.lttng2.kernel.ui.swtbot.tests.AllTests.class
+    org.eclipse.tracecompass.lttng2.kernel.ui.swtbot.tests.AllTests.class,
+    org.eclipse.tracecompass.lttng2.ust.ui.swtbot.tests.AllTests.class
 })
 public class RunAllSWTBotTests {
 
