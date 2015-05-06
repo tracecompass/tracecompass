bcd97f27068c99f52490e5d8cd033b63e98016d1
Marc-Andre Laperle <marc-andre.laperle@ericsson.com>
2015-05-06 17:28:36 -0400

    tmf: Fix regression of XML analysis combined with custom text parser not working
    bug: 466641
    Change-Id: I9c6ea6014e6f25f73e52ed182b6abc2ab2394a17
    Signed-off-by: Marc-Andre Laperle <marc-andre.laperle@ericsson.com>
    Reviewed-on: https://git.eclipse.org/r/47382
    Reviewed-by: Hudson CI
    Reviewed-by: Matthew Khouzam <matthew.khouzam@ericsson.com>
    Tested-by: Matthew Khouzam <matthew.khouzam@ericsson.com>

--- org.eclipse.tracecompass.tmf.core/src/org/eclipse/tracecompass/tmf/core/event/TmfEvent.java
+++ org.eclipse.tracecompass.tmf.core/src/org/eclipse/tracecompass/tmf/core/event/TmfEvent.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2014 Ericsson
+ * Copyright (c) 2009, 2015 Ericsson
  *
  * All rights reserved. This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License v1.0 which
@@ -141,7 +141,7 @@ public class TmfEvent extends PlatformObject implements ITmfEvent {
      */
     @Override
     public String getName() {
-        ITmfEventType type = fType;
+        ITmfEventType type = getType();
         if (type != null) {
             return type.getName();
         }
