5cd20891cb9952a78d38a1975ed07c7d6a2ec133
Marc-Andre Laperle <marc-andre.laperle@ericsson.com>
2015-06-18 15:08:20 -0400

    lttng: Set keyboard layout in SWTBot tests
    Depending on the locale of the system, some tests like testKeyboardLeftRight would fail with a NPE.
    
    Change-Id: If39e8faedffb8629b5cfd2b6e44a8bbd80b9990e
    Signed-off-by: Marc-Andre Laperle <marc-andre.laperle@ericsson.com>
    Reviewed-on: https://git.eclipse.org/r/50470
    Reviewed-by: Hudson CI
    Reviewed-by: Patrick Tasse <patrick.tasse@gmail.com>
    Tested-by: Patrick Tasse <patrick.tasse@gmail.com>

--- lttng/org.eclipse.tracecompass.lttng2.kernel.ui.swtbot.tests/src/org/eclipse/tracecompass/lttng2/kernel/ui/swtbot/tests/KernelTest.java
+++ lttng/org.eclipse.tracecompass.lttng2.kernel.ui.swtbot.tests/src/org/eclipse/tracecompass/lttng2/kernel/ui/swtbot/tests/KernelTest.java
@@ -66,6 +66,7 @@ public class KernelTest {
 
         /* set up for swtbot */
         SWTBotPreferences.TIMEOUT = 20000; /* 20 second timeout */
+        SWTBotPreferences.KEYBOARD_LAYOUT = "EN_US";
         fLogger.removeAllAppenders();
         fLogger.addAppender(new ConsoleAppender(new SimpleLayout(), ConsoleAppender.SYSTEM_OUT));
         fBot = new SWTWorkbenchBot();
