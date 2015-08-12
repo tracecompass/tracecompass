367e9cab4e15bf7bae1bb66a6a3e3cb65c3110a3
Bernd Hufmann <Bernd.Hufmann@ericsson.com>
2015-08-11 20:56:15 -0400

    control: Add support for downloading profiles after saving of sessions
    Change-Id: Ie42d8d2d408f52fd093780193859015cb0907613
    Signed-off-by: Bernd Hufmann <Bernd.Hufmann@ericsson.com>
    Reviewed-on: https://git.eclipse.org/r/57684
    Reviewed-by: Hudson CI
    Reviewed-by: Patrick Tasse <patrick.tasse@gmail.com>
    Tested-by: Patrick Tasse <patrick.tasse@gmail.com>

--- lttng/org.eclipse.tracecompass.lttng2.control.ui/src/org/eclipse/tracecompass/internal/lttng2/control/ui/views/handlers/SaveHandler.java
+++ lttng/org.eclipse.tracecompass.lttng2.control.ui/src/org/eclipse/tracecompass/internal/lttng2/control/ui/views/handlers/SaveHandler.java
@@ -11,23 +11,39 @@
  **********************************************************************/
 package org.eclipse.tracecompass.internal.lttng2.control.ui.views.handlers;
 
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.filesystem.EFS;
+import org.eclipse.core.filesystem.IFileStore;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.SubMonitor;
 import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.osgi.util.NLS;
+import org.eclipse.remote.core.IRemoteConnection;
+import org.eclipse.remote.core.IRemoteFileService;
+import org.eclipse.remote.core.IRemoteProcessService;
+import org.eclipse.remote.core.RemoteServicesUtils;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.tracecompass.internal.lttng2.control.core.LttngProfileManager;
 import org.eclipse.tracecompass.internal.lttng2.control.core.model.TraceSessionState;
 import org.eclipse.tracecompass.internal.lttng2.control.ui.Activator;
 import org.eclipse.tracecompass.internal.lttng2.control.ui.views.ControlView;
 import org.eclipse.tracecompass.internal.lttng2.control.ui.views.messages.Messages;
 import org.eclipse.tracecompass.internal.lttng2.control.ui.views.model.impl.TraceSessionComponent;
+import org.eclipse.tracecompass.internal.lttng2.control.ui.views.service.LTTngControlServiceConstants;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
@@ -73,8 +89,53 @@ public class SaveHandler extends BaseControlViewHandler {
                     try {
                         for (TraceSessionComponent session : sessions) {
                             session.saveSession(null, null, true, monitor);
+
+                            final IRemoteConnection connection = session.getTargetNode().getRemoteSystemProxy().getRemoteConnection();
+                            SubMonitor subMonitor = SubMonitor.convert(monitor, 3);
+                            // create destination directory (if necessary)
+                            IRemoteProcessService processService = connection.getService(IRemoteProcessService.class);
+                            IPath path = null;
+                            if (processService != null) {
+                                String cwd = processService.getWorkingDirectory();
+                                path = RemoteServicesUtils.posixPath(cwd);
+                                path = path.append(LTTngControlServiceConstants.DEFAULT_PATH);
+                            }
+
+                            if (path == null) {
+                                return Status.CANCEL_STATUS;
+                            }
+
+                            // upload file
+                            IRemoteFileService fileService = connection.getService(IRemoteFileService.class);
+                            if (fileService == null) {
+                                return Status.CANCEL_STATUS;
+                            }
+                            IPath dest = LttngProfileManager.getProfilePath();
+                            String profileName = session.getName() + ".lttng"; //$NON-NLS-1$
+                            final Path destPath = FileSystems.getDefault().getPath(dest.toString()).resolve(profileName);
+                            IFileStore destFileStore = EFS.getLocalFileSystem().fromLocalFile(destPath.toFile());
+                            SubMonitor childMonitor = subMonitor.newChild(1);
+
+                            IPath remotePath = RemoteServicesUtils.posixPath(path.toString()).append(profileName);
+                            IFileStore remoteResource = fileService.getResource(remotePath.toString());
+                            final boolean overwrite[] = new boolean[1];
+                            if (destPath.toFile().exists()) {
+                                Display.getDefault().syncExec(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        overwrite[0] = MessageDialog.openConfirm(Display.getDefault().getActiveShell(),
+                                                Messages.TraceControl_ProfileAlreadyExists,
+                                                NLS.bind(Messages.TraceControl_OverwriteQuery, destPath.getFileName()));
+
+                                    }
+                                });
+                                if (!overwrite[0]) {
+                                    continue;
+                                }
+                            }
+                            remoteResource.copy(destFileStore, EFS.OVERWRITE, childMonitor);
                         }
-                    } catch (ExecutionException e) {
+                    } catch (ExecutionException | CoreException e) {
                         return new Status(IStatus.ERROR, Activator.PLUGIN_ID, Messages.TraceControl_SaveFailure, e);
                     }
                     return Status.OK_STATUS;
