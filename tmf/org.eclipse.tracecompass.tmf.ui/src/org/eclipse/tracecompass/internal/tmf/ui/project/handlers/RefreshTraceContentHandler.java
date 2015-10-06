cb20ee13415624252da06e68bd673486b73467ca
Marc-Andre Laperle <marc-andre.laperle@ericsson.com>
2015-10-06 17:55:14 -0400

    tmf: Allow refreshing manually all ITmfTraceEditors and traces under experiment
    This is a follow-up to the comments in https://git.eclipse.org/r/#/c/57465
    
    Change-Id: Ia6e8ecc056a5c662255c35f029f72bbe1cc2785a
    Signed-off-by: Marc-Andre Laperle <marc-andre.laperle@ericsson.com>
    Reviewed-on: https://git.eclipse.org/r/57559
    Reviewed-by: Hudson CI
    Reviewed-by: Patrick Tasse <patrick.tasse@gmail.com>
    Tested-by: Patrick Tasse <patrick.tasse@gmail.com>
    Reviewed-on: https://git.eclipse.org/r/57734

--- tmf/org.eclipse.tracecompass.tmf.ui/src/org/eclipse/tracecompass/internal/tmf/ui/project/handlers/RefreshTraceContentHandler.java
+++ tmf/org.eclipse.tracecompass.tmf.ui/src/org/eclipse/tracecompass/internal/tmf/ui/project/handlers/RefreshTraceContentHandler.java
@@ -18,13 +18,13 @@ import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.tracecompass.tmf.core.event.ITmfEvent;
 import org.eclipse.tracecompass.tmf.core.signal.TmfTraceRangeUpdatedSignal;
 import org.eclipse.tracecompass.tmf.core.timestamp.TmfTimeRange;
 import org.eclipse.tracecompass.tmf.core.timestamp.TmfTimestamp;
 import org.eclipse.tracecompass.tmf.core.trace.ITmfTrace;
+import org.eclipse.tracecompass.tmf.ui.editors.ITmfTraceEditor;
 import org.eclipse.tracecompass.tmf.ui.project.model.TmfTraceElement;
-import org.eclipse.tracecompass.tmf.ui.project.model.TmfTraceFolder;
+import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.handlers.HandlerUtil;
 
 /**
@@ -41,30 +41,35 @@ public class RefreshTraceContentHandler extends AbstractHandler {
             return null;
         }
         final Iterator<Object> iterator = ((IStructuredSelection) selection).iterator();
-
+        boolean refreshed = false;
         while (iterator.hasNext()) {
             Object element = iterator.next();
             if (element instanceof TmfTraceElement) {
                 TmfTraceElement trace = (TmfTraceElement) element;
                 refreshTrace(trace);
-
-            } else if (element instanceof TmfTraceFolder) {
-                TmfTraceFolder tmfTraceFolder = (TmfTraceFolder) element;
-                for (TmfTraceElement e : tmfTraceFolder.getTraces()) {
-                    refreshTrace(e);
-                }
-            } else if (element instanceof ITmfEvent) {
-                ITmfEvent iTmfEvent = (ITmfEvent) element;
-                refreshTrace(iTmfEvent.getTrace());
+                refreshed = true;
             }
         }
+
+        if (refreshed) {
+            return null;
+        }
+
+        IEditorPart activeEditor = HandlerUtil.getActiveEditorChecked(event);
+        if (activeEditor instanceof ITmfTraceEditor) {
+            ITmfTrace trace = ((ITmfTraceEditor) activeEditor).getTrace();
+            refreshTrace(trace);
+        }
+
         return null;
     }
 
     private static void refreshTrace(TmfTraceElement t) {
         final TmfTraceElement traceElement = t.getElementUnderTraceFolder();
         ITmfTrace trace = traceElement.getTrace();
-        refreshTrace(trace);
+        if (trace != null) {
+            refreshTrace(trace);
+        }
     }
 
     private static void refreshTrace(ITmfTrace trace) {
