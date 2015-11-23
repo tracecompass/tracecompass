1124938ff7697af2cdcd2ada27706396861bdfa1
Jean-Christian Kouame <jean-christian.kouame@ericsson.com>
2015-11-23 17:44:45 -0500

    tmf : Remove inefficient new String(String) constructor
    Change-Id: I1ee9e4c969042e27219a756daeb9d43c210d23c1
    Signed-off-by: Jean-Christian Kouame <jean-christian.kouame@ericsson.com>
    Reviewed-on: https://git.eclipse.org/r/61085
    Reviewed-by: Hudson CI
    Reviewed-by: Matthew Khouzam <matthew.khouzam@ericsson.com>
    Tested-by: Matthew Khouzam <matthew.khouzam@ericsson.com>

--- tmf/org.eclipse.tracecompass.tmf.core/src/org/eclipse/tracecompass/tmf/core/analysis/TmfAnalysisModuleHelperConfigElement.java
+++ tmf/org.eclipse.tracecompass.tmf.core/src/org/eclipse/tracecompass/tmf/core/analysis/TmfAnalysisModuleHelperConfigElement.java
@@ -93,7 +93,7 @@ public class TmfAnalysisModuleHelperConfigElement implements IAnalysisModuleHelp
          * FIXME: No need to externalize this. A better solution will be found
          * soon and this string is just temporary
          */
-        return new String("The trace must be opened to get the help message"); //$NON-NLS-1$
+        return "The trace must be opened to get the help message"; //$NON-NLS-1$
     }
 
     @Override
