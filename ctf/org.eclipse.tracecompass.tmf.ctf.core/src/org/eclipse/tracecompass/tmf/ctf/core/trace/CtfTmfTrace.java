cbbe6286d98e9bd7813bc1f290a6a2c72d71af66
Matthew Khouzam <matthew.khouzam@ericsson.com>
2016-01-27 20:56:01 -0500

    ctf: expose UTC offset in trace properties
    This shows the time of boot of the system wrt UTC for
    most CTF traces. Initial step towards fixing bug 484620
    
    Change-Id: I62a934051f5d06c0e5c151743cbcc868f311e50f
    Signed-off-by: Matthew Khouzam <matthew.khouzam@ericsson.com>
    Reviewed-on: https://git.eclipse.org/r/65229
    Reviewed-by: Hudson CI
    Reviewed-by: Bernd Hufmann <bernd.hufmann@ericsson.com>
    Tested-by: Bernd Hufmann <bernd.hufmann@ericsson.com>
    Reviewed-on: https://git.eclipse.org/r/65604

--- ctf/org.eclipse.tracecompass.tmf.ctf.core/src/org/eclipse/tracecompass/tmf/ctf/core/trace/CtfTmfTrace.java
+++ ctf/org.eclipse.tracecompass.tmf.ctf.core/src/org/eclipse/tracecompass/tmf/ctf/core/trace/CtfTmfTrace.java
@@ -91,6 +91,12 @@ public class CtfTmfTrace extends TmfTrace
     // -------------------------------------------
 
     /**
+     * Clock offset property
+     * @since 1.2
+     */
+    public static final String CLOCK_OFFSET = "clock_offset"; //$NON-NLS-1$
+
+    /**
      * Default cache size for CTF traces
      */
     protected static final int DEFAULT_CACHE_SIZE = 50000;
@@ -444,6 +450,7 @@ public class CtfTmfTrace extends TmfTrace
     public Map<String, String> getTraceProperties() {
         Map<String, String> properties = new HashMap<>();
         properties.putAll(fTrace.getEnvironment());
+        properties.put(CLOCK_OFFSET, Long.toString(fTrace.getOffset()));
         properties.put(Messages.CtfTmfTrace_HostID, getHostId());
         return properties;
     }
