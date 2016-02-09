c5ff6bf3a02120452fba7f8a308cba8be91bf025
Geneviève Bastien <gbastien+lttng@versatic.net>
2016-02-08 21:50:13 -0500

    analysis.graph: allow workers to return additional information
    This information can be displayed as tooltips or extra columns in the views.
    
    Change-Id: Ib992ffa0d32b89c5811510e5a9a3270a1df1003d
    Signed-off-by: Matthew Khouzam <matthew.khouzam@ericsson.com>
    Signed-off-by: Geneviève Bastien <gbastien+lttng@versatic.net>
    Reviewed-on: https://git.eclipse.org/r/66060
    Reviewed-by: Hudson CI

--- analysis/org.eclipse.tracecompass.analysis.graph.core/src/org/eclipse/tracecompass/analysis/graph/core/base/IGraphWorker.java
+++ analysis/org.eclipse.tracecompass.analysis.graph.core/src/org/eclipse/tracecompass/analysis/graph/core/base/IGraphWorker.java
@@ -12,6 +12,9 @@
 
 package org.eclipse.tracecompass.analysis.graph.core.base;
 
+import java.util.Collections;
+import java.util.Map;
+
 /**
  * Interface that the objects in a graph may implement
  *
@@ -25,5 +28,31 @@ public interface IGraphWorker {
      * @return The host ID of the trace this worker belongs to
      */
     String getHostId();
+
+    /**
+     * Get additional information on this worker at time t. This would be
+     * textual information, in the form of key, value pairs, that could be
+     * displayed for instance as extra columns for this worker in a graph view.
+     *
+     * @return A key, value map of information this worker provides.
+     * @since 2.0
+     */
+    default Map<String, String> getWorkerInformation() {
+        return Collections.EMPTY_MAP;
+    }
+
+    /**
+     * Get additional information on this worker at time t. This would be
+     * textual information, in the form of key, value pairs, that could be
+     * displayed for instance as a tooltip in the graph view.
+     *
+     * @param t
+     *            Time at which to get the information
+     * @return A key, value map of information this worker provides.
+     * @since 2.0
+     */
+    default Map<String, String> getWorkerInformation(long t) {
+        return Collections.EMPTY_MAP;
+    }
 
 }
