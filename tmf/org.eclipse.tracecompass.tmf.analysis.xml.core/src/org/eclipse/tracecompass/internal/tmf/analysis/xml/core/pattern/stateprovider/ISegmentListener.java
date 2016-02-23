667601bf16c7e4743ee590f28d301a3662f8c3af
Jean-Christian Kouame <jean-christian.kouame@ericsson.com>
2016-02-23 18:58:19 -0500

    analysis : Introduce ISegmentListener
    This patch add a listener for segments creation
    
    Change-Id: I1da2cf3f6f91599ef4fc1a92f05306e90db0dc03
    Signed-off-by: Jean-Christian Kouame <jean-christian.kouame@ericsson.com>
    Reviewed-on: https://git.eclipse.org/r/67195
    Reviewed-by: Matthew Khouzam <matthew.khouzam@ericsson.com>
    Tested-by: Genevieve Bastien <gbastien+lttng@versatic.net>
    Reviewed-by: Genevieve Bastien <gbastien+lttng@versatic.net>
    Reviewed-by: Hudson CI

---
+++ tmf/org.eclipse.tracecompass.tmf.analysis.xml.core/src/org/eclipse/tracecompass/internal/tmf/analysis/xml/core/pattern/stateprovider/ISegmentListener.java
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Ericsson
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.tracecompass.internal.tmf.analysis.xml.core.pattern.stateprovider;
+
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.tracecompass.segmentstore.core.ISegment;
+
+/**
+ * Listener for segment creation.
+ * <p>
+ * Segment store analyses could build their store based on segments created by
+ * another analysis. In this case, the segment store analysis should implement
+ * this listener in order to receive the segments as they are created.
+ *
+ * @author Jean-Christian Kouame
+ *
+ */
+public interface ISegmentListener {
+
+    /**
+     * Called each time a segment will be created
+     *
+     * @param segment
+     *            The new segment
+     */
+    void onNewSegment(@NonNull ISegment segment);
+}
