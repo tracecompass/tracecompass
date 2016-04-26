c418e43e48cf4c36c1bc2bae78a1a11752b9db2e
Alexandre Montplaisir <alexmonthy@efficios.com>
2016-04-26 18:56:10 -0400

    analysis.lami: Add a plugin for core tests
    Change-Id: I49bb641ea07225d5036976593958da5400a302e0
    Signed-off-by: Alexandre Montplaisir <alexmonthy@efficios.com>
    Reviewed-on: https://git.eclipse.org/r/71567
    Reviewed-by: Hudson CI
    Reviewed-by: Bernd Hufmann <bernd.hufmann@ericsson.com>
    Tested-by: Bernd Hufmann <bernd.hufmann@ericsson.com>

---
+++ analysis/org.eclipse.tracecompass.analysis.lami.core.tests/src/org/eclipse/tracecompass/analysis/lami/core/tests/Activator.java
@@ -0,0 +1,45 @@
+/*******************************************************************************
+ * Copyright (c) 2016 EfficiOS Inc., Alexandre Montplaisir
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+
+package org.eclipse.tracecompass.analysis.lami.core.tests;
+
+import org.eclipse.tracecompass.common.core.TraceCompassActivator;
+
+/**
+ * Plugin activator
+ */
+public class Activator extends TraceCompassActivator {
+
+    private static final String PLUGIN_ID = "org.eclipse.tracecompass.analysis.lami.core.tests"; //$NON-NLS-1$
+
+    /**
+     * Return the singleton instance of this activator.
+     *
+     * @return The singleton instance
+     */
+    public static Activator instance() {
+        return (Activator) TraceCompassActivator.getInstance(PLUGIN_ID);
+    }
+
+    /**
+     * Constructor
+     */
+    public Activator() {
+        super(PLUGIN_ID);
+    }
+
+    @Override
+    protected void startActions() {
+    }
+
+    @Override
+    protected void stopActions() {
+    }
+
+}
---
+++ analysis/org.eclipse.tracecompass.analysis.lami.core.tests/src/org/eclipse/tracecompass/analysis/lami/core/tests/package-info.java
@@ -0,0 +1,11 @@
+/*******************************************************************************
+ * Copyright (c) 2016 EfficiOS Inc., Alexandre Montplaisir
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+
+@org.eclipse.jdt.annotation.NonNullByDefault
+package org.eclipse.tracecompass.analysis.lami.core.tests;
