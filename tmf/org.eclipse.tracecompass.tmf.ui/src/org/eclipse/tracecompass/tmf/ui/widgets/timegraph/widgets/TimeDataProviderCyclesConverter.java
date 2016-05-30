01591ae3c5a32090472c160039715e240c689520
Patrick Tasse <patrick.tasse@gmail.com>
2016-05-30 17:20:10 -0400

    tmf: Bug 494952: Remove deadlock in Time Chart view
    The handling of signal TmfTraceUpdated is now done in a
    ProcessTraceThread. There is only one such thread per trace active at a
    time. If an update is requested while a thread is active, the thread is
    marked to be restarted after its current iteration completion.
    
    The starting or restarting of a ProcessTraceThread is consolidated in a
    method and synchronized.
    
    The ProcessTraceThread now always processes the full time range of the
    trace, even when the view is opened with a trace already opened.
    
    A missing synchronization on DecorateThread concurrent access is added.
    
    Method ITimeDataProvider.resetStartFinishTime(boolean) is added to allow
    the TimeChartView to reset its window range without notifying listeners.
    
    Change-Id: I0d49d2712af4e5c645bd61bfde46ba37fc42779c
    Signed-off-by: Patrick Tasse <patrick.tasse@gmail.com>
    Reviewed-on: https://git.eclipse.org/r/73996
    Reviewed-by: Hudson CI
    Reviewed-by: Matthew Khouzam <matthew.khouzam@ericsson.com>
    Reviewed-by: Bernd Hufmann <bernd.hufmann@ericsson.com>
    Tested-by: Matthew Khouzam <matthew.khouzam@ericsson.com>
    (cherry picked from commit 7255573c13c43ecd96e204c52928db14c9bfe28c)
    Reviewed-on: https://git.eclipse.org/r/74698

--- tmf/org.eclipse.tracecompass.tmf.ui/src/org/eclipse/tracecompass/tmf/ui/widgets/timegraph/widgets/TimeDataProviderCyclesConverter.java
+++ tmf/org.eclipse.tracecompass.tmf.ui/src/org/eclipse/tracecompass/tmf/ui/widgets/timegraph/widgets/TimeDataProviderCyclesConverter.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2014, 2015 Ericsson
+ * Copyright (c) 2014, 2016 Ericsson
  *
  * All rights reserved. This program and the accompanying materials are
  * made available under the terms of the Eclipse Public License v1.0 which
@@ -157,6 +157,14 @@ public class TimeDataProviderCyclesConverter implements ITimeDataProviderConvert
     @Override
     public void resetStartFinishTime() {
         fProvider.resetStartFinishTime();
+    }
+
+    /**
+     * @since 2.0
+     */
+    @Override
+    public void resetStartFinishTime(boolean notify) {
+        fProvider.resetStartFinishTime(notify);
     }
 
     @Override
