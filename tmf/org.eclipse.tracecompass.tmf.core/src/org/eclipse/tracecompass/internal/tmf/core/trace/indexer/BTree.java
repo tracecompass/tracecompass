bd824de90728c1e2677064a8f48fd34a9ee1f11d
Patrick Tasse <patrick.tasse@gmail.com>
2016-06-02 17:10:00 -0400

    tmf: Bug 495219: Fix NPE in checkpoint indexer seeking on disposed trace
    In BTree, if it is disposed (file is null), return -1 (insertion point
    0) in the binary search. This will correspond to checkpoint 0 in the
    flat array.
    
    In FlatArray, if it is disposed (file is null) return null as checkpoint
    in get(long).
    
    In TmfCheckpointIndexer, if the returned checkpoint is null in
    restoreCheckpoint(long), return the context given by the trace
    implementation of seekEvent((ITmfLocation) null). It should properly
    handle a disposed trace.
    
    Change-Id: I3148d2e10118628e387d206054de55d5238a9807
    Signed-off-by: Patrick Tasse <patrick.tasse@gmail.com>
    Reviewed-on: https://git.eclipse.org/r/74474
    Reviewed-by: Hudson CI
    Reviewed-by: Matthew Khouzam <matthew.khouzam@ericsson.com>
    Tested-by: Matthew Khouzam <matthew.khouzam@ericsson.com>
    Reviewed-by: Marc-Andre Laperle <marc-andre.laperle@ericsson.com>
    Tested-by: Marc-Andre Laperle <marc-andre.laperle@ericsson.com>
    (cherry picked from commit 7b3400bd1c99cfd700b342a3563bd04ce6d5156e)
    Reviewed-on: https://git.eclipse.org/r/74818

--- tmf/org.eclipse.tracecompass.tmf.core/src/org/eclipse/tracecompass/internal/tmf/core/trace/indexer/BTree.java
+++ tmf/org.eclipse.tracecompass.tmf.core/src/org/eclipse/tracecompass/internal/tmf/core/trace/indexer/BTree.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013, 2014 Ericsson
+ * Copyright (c) 2013, 2016 Ericsson
  *
  * All rights reserved. This program and the accompanying materials are
  * made available under the terms of the Eclipse Public License v1.0 which
@@ -290,7 +290,7 @@ public class BTree extends AbstractFileCheckpointCollection {
 
     private void accept(long nodeOffset, IBTreeVisitor visitor) {
 
-        if (nodeOffset == BTreeNode.NULL_CHILD) {
+        if (nodeOffset == BTreeNode.NULL_CHILD || getRandomAccessFile() == null) {
             return;
         }
 
