c5f8acb46fc41cc3d7397811ff8371d8cfaf6c8e
Marc-Andre Laperle <marc-andre.laperle@ericsson.com>
2016-08-01 14:58:38 -0400

    releng: Bump versions for 2.0.1 release
    Also add scripts to help updating version numbers.
    
    Change-Id: Id22240cde48f56e8dc7d000ae6206c4ba7b5f4bd
    Signed-off-by: Marc-Andre Laperle <marc-andre.laperle@ericsson.com>
    Reviewed-on: https://git.eclipse.org/r/78283
    Reviewed-by: Hudson CI
    Reviewed-by: Matthew Khouzam <matthew.khouzam@ericsson.com>
    Reviewed-by: Bernd Hufmann <bernd.hufmann@ericsson.com>
    Tested-by: Bernd Hufmann <bernd.hufmann@ericsson.com>

---
+++ releng/update_version.sh
@@ -0,0 +1,39 @@
+###############################################################################
+# Copyright (c) 2016 Ericsson
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+###############################################################################
+
+oldVersion=$1
+newVersion=$2
+
+if [ -z "$oldVersion" -o -z "$newVersion" ]; then
+	echo "usage: $0 oldversion newversion"
+	exit 1
+fi
+
+echo Changing $oldVersion to $newVersion
+
+#Update root pom version
+find .. -maxdepth 1 -name "pom.xml" -exec python update_root_pom_versions.py {} $oldVersion $newVersion \;
+#Update pom.xml with <parent> tag with the new version of the root pom
+find .. -name "pom.xml" -type f -exec python update_parent_pom_versions.py {} $oldVersion $newVersion \;
+#Update doc plugin versions
+find ../doc -name "MANIFEST.MF" -exec sed -i -e s/$oldVersion.qualifier/$newVersion.qualifier/g {} \;
+
+#Update feature versions (feature.xml)
+find .. -name "feature.xml" -exec sed -i -e s/$oldVersion.qualifier/$newVersion.qualifier/g {} \;
+
+#Update branding plugin manifest.MF
+sed -i -e s/$oldVersion.qualifier/$newVersion.qualifier/g ../rcp/org.eclipse.tracecompass.rcp.branding/META-INF/MANIFEST.MF
+#rcp/org.eclipse.tracecompass.rcp.branding/plugin.xml aboutText
+sed -i -e s/$oldVersion/$newVersion/g ../rcp/org.eclipse.tracecompass.rcp.branding/plugin.xml
+
+#Update .product rcp/org.eclipse.tracecompass.rcp.product/tracing.product
+sed -i -e s/$oldVersion/$newVersion/g ../rcp/org.eclipse.tracecompass.rcp.product/tracing.product
+
+#Update rcp.ui plugin rcp/org.eclipse.tracecompass.rcp.ui/META-INF/MANIFEST.MF
+sed -i -e s/$oldVersion.qualifier/$newVersion.qualifier/g ../rcp/org.eclipse.tracecompass.rcp.ui/META-INF/MANIFEST.MF
