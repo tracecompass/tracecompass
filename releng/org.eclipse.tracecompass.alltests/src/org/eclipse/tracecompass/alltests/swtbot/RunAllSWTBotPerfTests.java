552d0e24ef79f04b2807a8ce305b1451337f1e20
Geneviève Bastien <gbastien+lttng@versatic.net>
2016-09-01 16:26:44 -0400

    releng: Add a test suite for all SWTbot perf tests
    These tests can be run with JUL enabled to get data for UI
    responsiveness.
    
    Change-Id: I87cf69155262089dfe017db5ad4980951308224e
    Signed-off-by: Geneviève Bastien <gbastien+lttng@versatic.net>
    Reviewed-on: https://git.eclipse.org/r/80262
    Reviewed-by: Hudson CI
    Reviewed-by: Marc-André Laperle <marc-andre.laperle@ericsson.com>
    Tested-by: Marc-André Laperle <marc-andre.laperle@ericsson.com>

---
+++ releng/org.eclipse.tracecompass.alltests/src/org/eclipse/tracecompass/alltests/swtbot/RunAllSWTBotPerfTests.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 2016 École Polytechnique de Montréal
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+
+package org.eclipse.tracecompass.alltests.swtbot;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Master test suite for all Trace Compass SWTBot performance tests.
+ *
+ * Note that the SWTBot tests need to be executed in a non-UI thread which is
+ * why they are separated in a different test suite.
+ *
+ * Also, these tests typically test the performance of the UI and one might want
+ * to enable JUL logging while running these.
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+    org.eclipse.tracecompass.lttng2.ust.ui.swtbot.tests.perf.LttngUstResponseBenchmark.class,
+    org.eclipse.tracecompass.lttng2.kernel.ui.swtbot.tests.perf.LttngUiResponseBenchmark.class
+})
+public class RunAllSWTBotPerfTests {
+
+}
