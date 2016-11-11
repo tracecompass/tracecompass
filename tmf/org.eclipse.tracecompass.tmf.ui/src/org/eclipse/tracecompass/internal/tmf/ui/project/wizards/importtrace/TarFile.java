60867d6adac129ab95ea38786e517abdc4bb3723
Marc-Andre Laperle <marc-andre.laperle@ericsson.com>
2016-11-11 00:56:43 -0500

    tmf: Bug 507415: Fix some random file wrongly seen as Tar
    Change-Id: Iff3ee2a5cad352f6b1f658e4da462dfe45e21418
    Signed-off-by: Marc-Andre Laperle <marc-andre.laperle@ericsson.com>
    Reviewed-on: https://git.eclipse.org/r/84852
    Reviewed-by: Bernd Hufmann <bernd.hufmann@ericsson.com>
    Tested-by: Bernd Hufmann <bernd.hufmann@ericsson.com>
    Reviewed-by: Hudson CI
    (cherry picked from commit c4d84203f070b4d05d9847f3a508da54cb3a724d)
    Reviewed-on: https://git.eclipse.org/r/84925

--- tmf/org.eclipse.tracecompass.tmf.ui/src/org/eclipse/tracecompass/internal/tmf/ui/project/wizards/importtrace/TarFile.java
+++ tmf/org.eclipse.tracecompass.tmf.ui/src/org/eclipse/tracecompass/internal/tmf/ui/project/wizards/importtrace/TarFile.java
@@ -63,6 +63,9 @@ public class TarFile {
         entryEnumerationStream = new TarArchiveInputStream(fInputStream);
         try {
             curEntry = (TarArchiveEntry) entryEnumerationStream.getNextEntry();
+            if (!curEntry.isCheckSumOK()) {
+                throw new IOException("Error detected parsing initial entry header"); //$NON-NLS-1$
+            }
         } catch (IOException e) {
             fInputStream.close();
             throw e;
