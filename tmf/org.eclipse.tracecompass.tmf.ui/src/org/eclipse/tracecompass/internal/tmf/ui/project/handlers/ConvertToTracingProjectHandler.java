315b68747e01332f5c692c3b335ad34777b81c66
Bernd Hufmann <Bernd.Hufmann@ericsson.com>
2017-01-17 13:44:49 -0500

    tmf: add command to add the tracing nature to any project
    The command is added as sub-menu of context-sensitive menu item
    "Configure" when right-mouse clicking on the project. 
    
    For that the entry "Convert to Tracing Project" is added to the
    "Configure" menu.
    
    Change-Id: I7d77642f640b1cb5a3ebb9c28ef1b711d2ef7794
    Signed-off-by: Bernd Hufmann <Bernd.Hufmann@ericsson.com>
    Reviewed-on: https://git.eclipse.org/r/88888
    Reviewed-by: Hudson CI
    Reviewed-by: Patrick Tasse <patrick.tasse@gmail.com>
    Tested-by: Patrick Tasse <patrick.tasse@gmail.com>

---
+++ tmf/org.eclipse.tracecompass.tmf.ui/src/org/eclipse/tracecompass/internal/tmf/ui/project/handlers/ConvertToTracingProjectHandler.java
@@ -0,0 +1,60 @@
+/*******************************************************************************
+ * Copyright (c) 2017 Ericsson
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+
+package org.eclipse.tracecompass.internal.tmf.ui.project.handlers;
+
+import java.lang.reflect.InvocationTargetException;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.tracecompass.internal.tmf.ui.Activator;
+import org.eclipse.tracecompass.tmf.ui.project.model.TmfProjectRegistry;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+/**
+ * Converts a project to a tracing project.
+ *
+ * @author Bernd Hufmann
+ */
+public class ConvertToTracingProjectHandler extends AbstractHandler {
+
+    @Override
+    public Object execute(ExecutionEvent event) throws ExecutionException {
+
+        ISelection currentSelection = HandlerUtil.getCurrentSelectionChecked(event);
+        if (currentSelection instanceof IStructuredSelection) {
+            Object firstElement = ((IStructuredSelection) currentSelection).getFirstElement();
+            if (firstElement instanceof IProject) {
+                IProject project = (IProject) firstElement;
+                try {
+                    PlatformUI.getWorkbench().getProgressService().run(false, false, new IRunnableWithProgress() {
+                        @Override
+                        public void run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
+                            TmfProjectRegistry.addTracingNature(project, monitor);
+                        }
+                    });
+                } catch (InvocationTargetException e) {
+                    Activator.getDefault().logError("Error adding tracing nature to project " + project.getName(), e); //$NON-NLS-1$
+                } catch (InterruptedException e) {
+                    // do nothing
+                }
+            }
+        }
+        return Status.OK_STATUS;
+    }
+
+}
