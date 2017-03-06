5d01fe34d3475736db19ed8b1ce4988d43d699f0
Patrick Tasse <patrick.tasse@gmail.com>
2017-03-06 14:22:26 -0500

    tmf: Add X11 color registry
    This adds a map of color name strings to RGB color values. It will allow
    users to identify colors by name in dialogs, configuration files, etc.
    
    Change-Id: I492292acfd864ebad418049e584c617c525f6106
    Signed-off-by: Patrick Tasse <patrick.tasse@gmail.com>
    Reviewed-on: https://git.eclipse.org/r/92209
    Reviewed-by: Hudson CI
    Reviewed-by: Bernd Hufmann <bernd.hufmann@ericsson.com>
    Tested-by: Bernd Hufmann <bernd.hufmann@ericsson.com>

---
+++ tmf/org.eclipse.tracecompass.tmf.ui/src/org/eclipse/tracecompass/tmf/ui/colors/X11Color.java
@@ -0,0 +1,69 @@
+/*******************************************************************************
+ * Copyright (c) 2017 Ericsson
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Patrick Tasse - Initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.tracecompass.tmf.ui.colors;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.eclipse.swt.graphics.RGB;
+import org.eclipse.tracecompass.internal.tmf.ui.Activator;
+
+/**
+ * A registry of named X11 colors. It maps certain strings to RGB color values.
+ *
+ * @see <a href=
+ *      "http://cgit.freedesktop.org/xorg/app/rgb/tree/rgb.txt">http://cgit.freedesktop.org/xorg/app/rgb/tree/rgb.txt</a>
+ *
+ * @since 2.3
+ */
+public class X11Color {
+
+    private static final String X11_COLOR_FILE = "share/rgb.txt"; //$NON-NLS-1$
+    private static final Pattern PATTERN = Pattern.compile("\\s*(\\d{1,3})\\s*(\\d{1,3})\\s*(\\d{1,3})\\s*(.*\\S)\\s*"); //$NON-NLS-1$
+    private static final Map<String, RGB> COLORS = new HashMap<>();
+    static {
+        URL url = Activator.getDefault().getBundle().getEntry(X11_COLOR_FILE);
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(url.openConnection().getInputStream()))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                Matcher matcher = PATTERN.matcher(line);
+                if (matcher.matches()) {
+                    int r = Integer.parseInt(matcher.group(1));
+                    int g = Integer.parseInt(matcher.group(2));
+                    int b = Integer.parseInt(matcher.group(3));
+                    RGB rgb = new RGB(r, g, b);
+                    COLORS.put(matcher.group(4).toLowerCase(), rgb);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Get the RGB corresponding to a X11 color name.
+     *
+     * @param name
+     *            the X11 color name (case insensitive)
+     * @return the corresponding RGB, or null
+     */
+    public static RGB toRGB(String name) {
+        return COLORS.get(name.toLowerCase());
+    }
+}
