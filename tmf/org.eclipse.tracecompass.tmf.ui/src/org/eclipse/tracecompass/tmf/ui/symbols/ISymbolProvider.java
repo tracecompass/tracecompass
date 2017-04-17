35d62dbfcfc57a91adf4ef6108d452fd739650b2
Matthew Khouzam <matthew.khouzam@ericsson.com>
2017-04-16 22:10:34 -0400

    tmf: Split ISymbolProvider into core and UI parts
    The ISymbolProvider interface and related classes were all living in the UI
    plugin only because of the preference page option, which is a UI-only
    construct. This limitation was known since the inception of the symbol
    provider, with the intent of eventually correctly splitting it into
    core and UI components. This patch partially fullfills that.
    
    Future work:
    
    * Make symbol providers in core.
    
    Change-Id: I64696929fcd8f47ba34b6a7b48d5e814faca7616
    Signed-off-by: Matthew Khouzam <matthew.khouzam@ericsson.com>
    Reviewed-on: https://git.eclipse.org/r/95115
    Reviewed-by: Genevieve Bastien <gbastien+lttng@versatic.net>
    Tested-by: Genevieve Bastien <gbastien+lttng@versatic.net>
    Reviewed-by: Hudson CI

--- tmf/org.eclipse.tracecompass.tmf.ui/src/org/eclipse/tracecompass/tmf/ui/symbols/ISymbolProvider.java
+++ tmf/org.eclipse.tracecompass.tmf.ui/src/org/eclipse/tracecompass/tmf/ui/symbols/ISymbolProvider.java
@@ -10,49 +10,22 @@
 
 package org.eclipse.tracecompass.tmf.ui.symbols;
 
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.jdt.annotation.Nullable;
 import org.eclipse.tracecompass.tmf.core.event.lookup.ITmfCallsite;
-import org.eclipse.tracecompass.tmf.core.trace.ITmfTrace;
 import org.eclipse.tracecompass.tmf.core.trace.TmfTrace;
 
 /**
  * An ISymbolProvider is used to map symbol addresses that might be found inside
- * an {@link TmfTrace} into human readable strings.
+ * an {@link TmfTrace} into human readable strings. This interface should be
+ * used to augment
+ * {@link org.eclipse.tracecompass.tmf.core.symbols.ISymbolProvider} to support
+ * preference pages.
  *
  * @author Robert Kiss
  * @since 2.0
  * @see ISymbolProviderFactory
  */
-public interface ISymbolProvider {
-
-    /**
-     * @return the trace that this class resolves symbols for
-     */
-    @NonNull ITmfTrace getTrace();
-
-    /**
-     * Some providers might have configurations that take some time to load. All
-     * the CPU intensive load operations shall be done in this method. The
-     * adopters shall call this method at an opportune moment when cancellation
-     * and UI feedback is possible. However, the implementors of this interface
-     * shall not assume that this method has been called.
-     *
-     * @param monitor
-     *            The progress monitor to use, can be null
-     */
-    void loadConfiguration(IProgressMonitor monitor);
-
-    /**
-     * Return the symbol text corresponding to the given address or null if
-     * there is no such symbol
-     *
-     * @param address
-     *            the address of the symbol
-     * @return the symbol text or null if the symbol cannot be found
-     */
-    @Nullable String getSymbolText(long address);
+public interface ISymbolProvider extends org.eclipse.tracecompass.tmf.core.symbols.ISymbolProvider {
 
     /**
      * Return additional information regarding the symbol from the given address
@@ -67,22 +40,6 @@ public interface ISymbolProvider {
      */
     @Deprecated
     @Nullable ITmfCallsite getSymbolInfo(long address);
-
-    /**
-     * Return the symbol text corresponding to the given pid/timestamp/address
-     * tuple, or null if there is no such symbol.
-     *
-     * @param pid
-     *            The process Id for which to query
-     * @param timestamp
-     *            The timestamp of the query
-     * @param address
-     *            the address of the symbol
-     * @return the symbol text or null if the symbol cannot be found
-     */
-    default @Nullable String getSymbolText(int pid, long timestamp, long address) {
-        return getSymbolText(address);
-    }
 
     /**
      * Return additional information regarding the symbol from the given
