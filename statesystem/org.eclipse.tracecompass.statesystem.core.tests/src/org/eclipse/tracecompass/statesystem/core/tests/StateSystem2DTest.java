df6a91c3529dd06008fae01250e0a95beb43e9fb
Matthew Khouzam <matthew.khouzam@ericsson.com>
2017-07-20 23:19:06 -0400

    ss.test: Test 2D query fixes
    Test out of range and empty queries.
    
    Change-Id: Ia7e7a00d8e7063d7d00ac63e02eb0a05b08f6221
    Signed-off-by: Matthew Khouzam <matthew.khouzam@ericsson.com>
    Reviewed-on: https://git.eclipse.org/r/101697
    Reviewed-by: Hudson CI

--- statesystem/org.eclipse.tracecompass.statesystem.core.tests/src/org/eclipse/tracecompass/statesystem/core/tests/StateSystem2DTest.java
+++ statesystem/org.eclipse.tracecompass.statesystem.core.tests/src/org/eclipse/tracecompass/statesystem/core/tests/StateSystem2DTest.java
@@ -10,9 +10,9 @@
 package org.eclipse.tracecompass.statesystem.core.tests;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 import java.io.File;
 import java.io.IOException;
@@ -32,15 +32,14 @@ import org.eclipse.tracecompass.statesystem.core.backend.IStateHistoryBackend;
 import org.eclipse.tracecompass.statesystem.core.backend.StateHistoryBackendFactory;
 import org.eclipse.tracecompass.statesystem.core.exceptions.AttributeNotFoundException;
 import org.eclipse.tracecompass.statesystem.core.exceptions.StateSystemDisposedException;
-import org.eclipse.tracecompass.statesystem.core.exceptions.StateValueTypeException;
+import org.eclipse.tracecompass.statesystem.core.exceptions.TimeRangeException;
 import org.eclipse.tracecompass.statesystem.core.interval.ITmfStateInterval;
-import org.eclipse.tracecompass.statesystem.core.statevalue.TmfStateValue;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Iterables;
 import com.google.common.collect.Multimap;
 import com.google.common.collect.Ordering;
 import com.google.common.collect.TreeMultimap;
@@ -60,35 +59,29 @@ public class StateSystem2DTest {
 
     /**
      * Build a small state history tree
+     *
+     * @throws IOException
+     *             If the state system file could not be created
      */
     @Before
-    public void setupStateSystem() {
-        try {
-            IStateHistoryBackend backend = null;
-            try {
-                backend = StateHistoryBackendFactory.createHistoryTreeBackendNewFile("test",
-                        NonNullUtils.checkNotNull(File.createTempFile("2Dtest", "ht")), 0, START_TIME, 0);
-            } catch (IOException e) {
-                fail(e.getMessage());
-            }
-            fStateSystem = StateSystemFactory.newStateSystem(NonNullUtils.checkNotNull(backend));
-            int stringQuark = fStateSystem.getQuarkAbsoluteAndAdd(STRING_ATTRIBUTE);
-            int integerQuark = fStateSystem.getQuarkAbsoluteAndAdd(INTEGER_ATTRIBUTE);
+    public void setupStateSystem() throws IOException {
+        IStateHistoryBackend backend = StateHistoryBackendFactory.createHistoryTreeBackendNewFile("test",
+                NonNullUtils.checkNotNull(File.createTempFile("2Dtest", "ht")), 0, START_TIME, 0);
+        fStateSystem = StateSystemFactory.newStateSystem(NonNullUtils.checkNotNull(backend));
+        int stringQuark = fStateSystem.getQuarkAbsoluteAndAdd(STRING_ATTRIBUTE);
+        int integerQuark = fStateSystem.getQuarkAbsoluteAndAdd(INTEGER_ATTRIBUTE);
 
-            fStateSystem.modifyAttribute(60L, TmfStateValue.newValueString("String1"), stringQuark);
-            fStateSystem.modifyAttribute(70L, TmfStateValue.newValueInt(0), integerQuark);
-            fStateSystem.modifyAttribute(80L, TmfStateValue.newValueInt(1), integerQuark);
-            fStateSystem.modifyAttribute(90L, TmfStateValue.newValueString("String2"), stringQuark);
-            fStateSystem.modifyAttribute(100L, TmfStateValue.newValueInt(2), integerQuark);
-            fStateSystem.modifyAttribute(110L, TmfStateValue.newValueInt(3), integerQuark);
-            fStateSystem.modifyAttribute(130L, TmfStateValue.newValueString("String3"), stringQuark);
-            fStateSystem.modifyAttribute(140L, TmfStateValue.newValueString("String4"), stringQuark);
-            fStateSystem.modifyAttribute(160L, TmfStateValue.newValueInt(4), integerQuark);
+        fStateSystem.modifyAttribute(60L, "String1", stringQuark);
+        fStateSystem.modifyAttribute(70L, 0, integerQuark);
+        fStateSystem.modifyAttribute(80L, 1, integerQuark);
+        fStateSystem.modifyAttribute(90L, "String2", stringQuark);
+        fStateSystem.modifyAttribute(100L, 2, integerQuark);
+        fStateSystem.modifyAttribute(110L, 3, integerQuark);
+        fStateSystem.modifyAttribute(130L, "String3", stringQuark);
+        fStateSystem.modifyAttribute(140L, "String4", stringQuark);
+        fStateSystem.modifyAttribute(160L, 4, integerQuark);
 
-            fStateSystem.closeHistory(200L);
-        } catch (StateValueTypeException e) {
-            fail(e.getMessage());
-        }
+        fStateSystem.closeHistory(200L);
     }
 
     /**
@@ -104,64 +97,63 @@ public class StateSystem2DTest {
         Multimap<Integer, ITmfStateInterval> treeMap = TreeMultimap.create(Comparator.naturalOrder(),
                 Comparator.comparing(ITmfStateInterval::getStartTime));
 
-        /* Assert that intervals are distinct and sort them */
-        iterable.forEach(interval -> assertTrue(treeMap.put(interval.getAttribute(), interval)));
-        /* Check the number of distinct elements */
-        assertEquals(totalCount, treeMap.size());
-        /* There should only be as many Sets of intervals as quarks */
-        assertEquals(quarks.size(), treeMap.keySet().size());
+        iterable.forEach(interval -> assertTrue("Interval: " + interval + " was already returned: " + treeMap,
+                treeMap.put(interval.getAttribute(), interval)));
+        assertEquals("Wrong number of intervals returned", totalCount, treeMap.size());
+        assertEquals("There should only be as many Sets of intervals as quarks",
+                quarks.size(), treeMap.keySet().size());
 
         for (Integer quark : quarks) {
             Collection<ITmfStateInterval> orderedSet = treeMap.get(quark);
-            /* There should be intervals for this quark */
-            assertTrue(!orderedSet.isEmpty());
+            assertFalse("There should be intervals for quark: " + quark, orderedSet.isEmpty());
             ITmfStateInterval previous = null;
             for (ITmfStateInterval interval : orderedSet) {
                 if (previous == null) {
-                    /* Assert that the first interval intersects start. */
-                    assertTrue(interval.intersects(start));
+                    assertTrue("The first interval: " + interval + "should intersect start: " + start, interval.intersects(start));
                 } else {
-                    /*
-                     * Assert that this interval is contiguous to the previous
-                     * one.
-                     */
-                    assertEquals(previous.getEndTime() + 1, interval.getStartTime());
+                    assertEquals("Current interval: " + interval + " should have been contiguous to the previous one " + previous,
+                            previous.getEndTime() + 1, interval.getStartTime());
                 }
                 previous = interval;
             }
-            /* Assert that the last interval intersects end. */
-            assertNotNull(previous);
-            assertTrue(previous.intersects(end));
+            assertNotNull("There should have been at least one interval for quark " + quark, previous);
+            assertTrue("last interval: " + previous + " should intersect end " + end, previous.intersects(end));
         }
     }
 
     /**
      * Test the continuous 2D query method.
+     *
+     * @throws AttributeNotFoundException
+     *             if the requested attribute simply did not exist in the system.
+     * @throws StateSystemDisposedException
+     *             If the query is sent after the state system has been disposed
+     * @throws TimeRangeException
+     *             If the smallest time is before the state system start time.
+     * @throws IndexOutOfBoundsException
+     *             If the smallest attribute is <0 or if the largest is >= to the
+     *             number of attributes.
      */
     @Test
-    public void testContinuous2DQuery() {
+    public void testContinuous2DQuery() throws AttributeNotFoundException, IndexOutOfBoundsException, TimeRangeException, StateSystemDisposedException {
         ITmfStateSystem ss = fStateSystem;
         assertNotNull(ss);
         long end = ss.getCurrentEndTime();
 
-        try {
-            /* Make sure all and only the String intervals are returned */
-            int stringQuark = fStateSystem.getQuarkAbsolute(STRING_ATTRIBUTE);
-            Iterable<ITmfStateInterval> iterable = ss.query2D(Collections.singleton(stringQuark), START_TIME, end);
-            testContinuous(iterable, Collections.singleton(stringQuark), START_TIME, end, 5);
+        /* Make sure all and only the String intervals are returned */
+        int stringQuark = fStateSystem.getQuarkAbsolute(STRING_ATTRIBUTE);
+        Iterable<ITmfStateInterval> iterable = ss.query2D(Collections.singleton(stringQuark), START_TIME, end);
+        testContinuous(iterable, Collections.singleton(stringQuark), START_TIME, end, 5);
 
-            /* Make sure all and only the Integer intervals are returned */
-            int integerQuark = fStateSystem.getQuarkAbsolute(INTEGER_ATTRIBUTE);
-            iterable = ss.query2D(Collections.singleton(integerQuark), START_TIME, end);
-            testContinuous(iterable, Collections.singleton(integerQuark), START_TIME, end, 6);
+        /* Make sure all and only the Integer intervals are returned */
+        int integerQuark = fStateSystem.getQuarkAbsolute(INTEGER_ATTRIBUTE);
+        iterable = ss.query2D(Collections.singleton(integerQuark), START_TIME, end);
+        testContinuous(iterable, Collections.singleton(integerQuark), START_TIME, end, 6);
 
-            /* Make sure all intervals are returned */
-            Collection<Integer> quarks = ImmutableList.of(stringQuark, integerQuark);
-            iterable = ss.query2D(quarks, START_TIME, end);
-            testContinuous(iterable, quarks, START_TIME, end, 11);
-        } catch (AttributeNotFoundException | StateSystemDisposedException e) {
-            fail(e.getMessage());
-        }
+        /* Make sure all intervals are returned */
+        Collection<Integer> quarks = ImmutableList.of(stringQuark, integerQuark);
+        iterable = ss.query2D(quarks, START_TIME, end);
+        testContinuous(iterable, quarks, START_TIME, end, 11);
     }
 
     private static void testDiscrete(Iterable<ITmfStateInterval> iterable, Collection<Integer> quarks, Collection<Long> times, int totalCount) {
@@ -170,27 +162,33 @@ public class StateSystem2DTest {
 
         for (ITmfStateInterval interval : iterable) {
             assertTrue(quarks.contains(interval.getAttribute()));
+            assertTrue("interval: " + interval + " was returned twice", set.add(interval));
 
-            /* Assert that intervals are distinct */
-            assertTrue(set.add(interval));
-
-            /* Count how many time stamps this interval overlaps. */
             int timeStamps = (int) times.stream().filter(interval::intersects).count();
-            assertTrue(timeStamps > 0);
+            assertTrue("interval: " + interval + " does not intersect any time stamp: " + times, timeStamps > 0);
             countTimeStamps += timeStamps;
         }
 
-        /* Check the number of distinct elements */
-        assertEquals(totalCount, set.size());
-        /* Check that all time stamps are covered */
-        assertEquals(times.size() * quarks.size(), countTimeStamps);
+        assertEquals("incorrect number of intervals returned", totalCount, set.size());
+        assertEquals("All the queried time stamps were not covered", times.size() * quarks.size(), countTimeStamps);
     }
 
     /**
      * Test the discrete 2D query method.
+     *
+     * @throws AttributeNotFoundException
+     *             if the requested attribute simply did not exist in the system.
+     * @throws StateSystemDisposedException
+     *             If the query is sent after the state system has been disposed
+     * @throws TimeRangeException
+     *             If the smallest time is before the state system start time.
+     * @throws IndexOutOfBoundsException
+     *             If the smallest attribute is <0 or if the largest is >= to the
+     *             number of attributes.
      */
     @Test
-    public void testDiscrete2DQuery() {
+    public void testDiscrete2DQuery() throws AttributeNotFoundException,
+            IndexOutOfBoundsException, TimeRangeException, StateSystemDisposedException {
         ITmfStateSystem ss = fStateSystem;
         assertNotNull(ss);
         long end = ss.getCurrentEndTime();
@@ -198,24 +196,128 @@ public class StateSystem2DTest {
         assertEquals(6, times.size());
         assertTrue(Ordering.natural().isStrictlyOrdered(times));
 
-        try {
-            /* Make sure all and only the String intervals are returned */
-            int stringQuark = fStateSystem.getQuarkAbsolute(STRING_ATTRIBUTE);
-            Iterable<ITmfStateInterval> iterable = ss.query2D(Collections.singleton(stringQuark), times);
-            testDiscrete(iterable, Collections.singleton(stringQuark), times, 4);
+        /* Make sure all and only the String intervals are returned */
+        int stringQuark = fStateSystem.getQuarkAbsolute(STRING_ATTRIBUTE);
+        Iterable<ITmfStateInterval> iterable = ss.query2D(Collections.singleton(stringQuark), times);
+        testDiscrete(iterable, Collections.singleton(stringQuark), times, 4);
 
-            /* Make sure all and only the Integer intervals are returned */
-            int integerQuark = fStateSystem.getQuarkAbsolute(INTEGER_ATTRIBUTE);
-            iterable = ss.query2D(Collections.singleton(integerQuark), times);
-            testDiscrete(iterable, Collections.singleton(integerQuark), times, 4);
+        /* Make sure all and only the Integer intervals are returned */
+        int integerQuark = fStateSystem.getQuarkAbsolute(INTEGER_ATTRIBUTE);
+        iterable = ss.query2D(Collections.singleton(integerQuark), times);
+        testDiscrete(iterable, Collections.singleton(integerQuark), times, 4);
 
-            /* Make sure all intervals are returned */
-            Collection<Integer> quarks = ImmutableList.of(stringQuark, integerQuark);
-            iterable = ss.query2D(quarks, times);
-            testDiscrete(iterable, quarks, times, 8);
-        } catch (AttributeNotFoundException | StateSystemDisposedException e) {
-            fail(e.getMessage());
-        }
+        /* Make sure all intervals are returned */
+        Collection<Integer> quarks = ImmutableList.of(stringQuark, integerQuark);
+        iterable = ss.query2D(quarks, times);
+        testDiscrete(iterable, quarks, times, 8);
+    }
+
+    /**
+     * Test index out of bound queries
+     *
+     * @throws StateSystemDisposedException
+     *             ss was closed
+     * @throws TimeRangeException
+     *             time was out of range
+     * @throws IndexOutOfBoundsException
+     *             queried an attribute that was out of bounds
+     */
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testIOOB2DQuery() throws IndexOutOfBoundsException, TimeRangeException, StateSystemDisposedException {
+        ITmfStateSystem ss = fStateSystem;
+        assertNotNull(ss);
+        Collection<Long> times = Collections.singleton(77L);
+
+        int stringQuark = Integer.MAX_VALUE;
+        ss.query2D(Collections.singleton(stringQuark), times);
+    }
+
+    /**
+     * Test a too low time range exception on continuous queries
+     *
+     * @throws StateSystemDisposedException
+     *             ss was closed
+     * @throws TimeRangeException
+     *             time was out of range
+     * @throws IndexOutOfBoundsException
+     *             queried an attribute that was out of bounds
+     */
+    @Test(expected = TimeRangeException.class)
+    public void testTimeRangeException2DContinous() throws IndexOutOfBoundsException, TimeRangeException, StateSystemDisposedException {
+        ITmfStateSystem ss = fStateSystem;
+        assertNotNull(ss);
+
+        ss.query2D(Collections.singleton(0), 0L, 77L);
+    }
+
+    /**
+     * Test a too low time range exception on discrete queries
+     *
+     * @throws StateSystemDisposedException
+     *             ss was closed
+     * @throws TimeRangeException
+     *             time was out of range
+     * @throws IndexOutOfBoundsException
+     *             queried an attribute that was out of bounds
+     */
+    @Test(expected = TimeRangeException.class)
+    public void testTimeRangeException2DDiscrete() throws IndexOutOfBoundsException, TimeRangeException, StateSystemDisposedException {
+        ITmfStateSystem ss = fStateSystem;
+        assertNotNull(ss);
+
+        ss.query2D(Collections.singleton(0), Collections.singleton(Long.MIN_VALUE));
+    }
+
+    /**
+     * Test Empty queries
+     *
+     * @throws StateSystemDisposedException
+     *             ss was closed
+     * @throws TimeRangeException
+     *             time was out of range
+     * @throws IndexOutOfBoundsException
+     *             queried an attribute that was out of bounds
+     */
+    @Test
+    public void testEmpty2DQuery() throws IndexOutOfBoundsException, TimeRangeException, StateSystemDisposedException {
+        ITmfStateSystem ss = fStateSystem;
+        assertNotNull(ss);
+
+        /* Test on a discrete query */
+        Iterable<ITmfStateInterval> iterable = ss.query2D(Collections.emptyList(), Collections.singleton(77L));
+        assertNotNull(iterable);
+        assertTrue(Iterables.isEmpty(iterable));
+
+        /* Test on an empty time sample */
+        iterable = ss.query2D(Collections.singleton(0), Collections.emptyList());
+        assertNotNull(iterable);
+        assertTrue(Iterables.isEmpty(iterable));
+
+        /* Test on a continuous query */
+        iterable = ss.query2D(Collections.emptyList(), 77L, 78L);
+        assertNotNull(iterable);
+        assertTrue(Iterables.isEmpty(iterable));
+    }
+
+    /**
+     * Test Illegal time bounds exception
+     *
+     * @throws StateSystemDisposedException
+     *             ss was closed
+     * @throws TimeRangeException
+     *             time was out of range
+     * @throws IndexOutOfBoundsException
+     *             queried an attribute that was out of bounds
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void testIllegalBounds() throws IndexOutOfBoundsException, TimeRangeException, StateSystemDisposedException {
+        ITmfStateSystem ss = fStateSystem;
+        assertNotNull(ss);
+
+        /* Test on a continuous query */
+        Iterable<@NonNull ITmfStateInterval> iterable = ss.query2D(Collections.emptyList(), 80L, 78L);
+        assertNotNull(iterable);
+        assertTrue(Iterables.isEmpty(iterable));
     }
 
 }
