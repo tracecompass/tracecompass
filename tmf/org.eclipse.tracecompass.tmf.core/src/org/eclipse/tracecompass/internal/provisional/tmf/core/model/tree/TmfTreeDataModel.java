d1b182b1ff433cd8ad3df2d9a67a45f24765b511
Yonni Chen <yonni.chen.kuang.piao@ericsson.com>
2017-08-28 16:32:44 -0400

    tmf.core : Introduce tree data model and data provider
    - Tree model doesn't have a direct reference to its parent and children.
    It encapsulates only the id. It will be viewer/client responsibility to
    rebuild a tree hierarchy if needed
    
    Change-Id: I7787cd595ac878b61d1680315d683843ed2b575a
    Signed-off-by: Yonni Chen <yonni.chen.kuang.piao@ericsson.com>
    Reviewed-on: https://git.eclipse.org/r/100740
    Reviewed-by: Hudson CI
    Reviewed-by: Loic Prieur-Drevon <loic.prieurdrevon@gmail.com>
    Tested-by: Loic Prieur-Drevon <loic.prieurdrevon@gmail.com>
    Reviewed-by: Jean-Christian Kouame <jean-christian.kouame@ericsson.com>

---
+++ tmf/org.eclipse.tracecompass.tmf.core/src/org/eclipse/tracecompass/internal/provisional/tmf/core/model/tree/TmfTreeDataModel.java
@@ -0,0 +1,53 @@
+/**********************************************************************
+ * Copyright (c) 2017 Ericsson
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ **********************************************************************/
+
+package org.eclipse.tracecompass.internal.provisional.tmf.core.model.tree;
+
+/**
+ * Basic implementation of {@link ITmfTreeDataModel}.
+ *
+ * @author Yonni Chen
+ */
+public class TmfTreeDataModel implements ITmfTreeDataModel {
+
+    private final long fId;
+    private final long fParentId;
+    private final String fName;
+
+    /**
+     * Constructor
+     *
+     * @param id
+     *            The id of the model
+     * @param parentId
+     *            The parent id of this model. If it has none, give <code>-1</code>.
+     * @param name
+     *            The name of this model
+     */
+    public TmfTreeDataModel(long id, long parentId, String name) {
+        fId = id;
+        fParentId = parentId;
+        fName = name;
+    }
+
+    @Override
+    public long getId() {
+        return fId;
+    }
+
+    @Override
+    public long getParentId() {
+        return fParentId;
+    }
+
+    @Override
+    public String getName() {
+        return fName;
+    }
+}
