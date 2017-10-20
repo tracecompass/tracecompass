be7b0b238537bfe8c1cc03b2d7275100823ac24f
Geneviève Bastien <gbastien+lttng@versatic.net>
2017-10-20 10:11:54 -0400

    rcp.incubator: Add a menu to open a bugzilla page
    This adds a menu under the "Incubator" menu to give feedback and
    report bugs. This will open the new bug page in bugzilla in the
    user's default browser.
    
    Change-Id: I6039ce769c3f647b379a115e46475c27f3b62bbf
    Signed-off-by: Geneviève Bastien <gbastien+lttng@versatic.net>
    Reviewed-on: https://git.eclipse.org/r/110460
    Reviewed-by: Hudson CI
    Reviewed-by: Matthew Khouzam <matthew.khouzam@ericsson.com>
    Tested-by: Matthew Khouzam <matthew.khouzam@ericsson.com>

---
+++ rcp/org.eclipse.tracecompass.rcp.incubator.ui/src/org/eclipse/tracecompass/internal/rcp/incubator/ui/GiveFeedbackHandler.java
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * Copyright (c) 2017 École Polytechnique de Montréal
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+
+package org.eclipse.tracecompass.internal.rcp.incubator.ui;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.swt.program.Program;
+
+/**
+ * @author Geneviève Bastien
+ */
+public class GiveFeedbackHandler extends AbstractHandler {
+
+    private static final String URL = "https://bugs.eclipse.org/bugs/enter_bug.cgi?product=Tracecompass.Incubator"; //$NON-NLS-1$
+
+    @Override
+    public Object execute(ExecutionEvent event) throws ExecutionException {
+        Program.launch(URL);
+        return null;
+    }
+
+}
