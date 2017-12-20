daf78be37b73f7aa07e1b6712db32cf8bf99d9b3
Bernd Hufmann <Bernd.Hufmann@ericsson.com>
2017-12-20 15:42:45 -0500

    tmf: Bug 528919: don't refresh TmfExperimentFolder in TmfTraceElement
    This is done in the resource change handler of TmfProjectRegistry.
    
    Doing the refresh here will refresh the children as well as the
    viewer. Removing the call will decrease the time when deleting
    many traces while many experiments exist.
    
    Change-Id: Ibafbad3762b7fbb45561d570016653d2e27502b6
    Signed-off-by: Bernd Hufmann <Bernd.Hufmann@ericsson.com>
    Reviewed-on: https://git.eclipse.org/r/114516
    Reviewed-by: Hudson CI
    Reviewed-by: Matthew Khouzam <matthew.khouzam@ericsson.com>
    Tested-by: Matthew Khouzam <matthew.khouzam@ericsson.com>

--- tmf/org.eclipse.tracecompass.tmf.ui/src/org/eclipse/tracecompass/tmf/ui/project/model/TmfTraceElement.java
+++ tmf/org.eclipse.tracecompass.tmf.ui/src/org/eclipse/tracecompass/tmf/ui/project/model/TmfTraceElement.java
@@ -759,7 +759,6 @@ public class TmfTraceElement extends TmfCommonProjectElement implements IActionF
                         if (!toRemove.isEmpty() && experiment.getTraces().isEmpty()) {
                             // If experiment becomes empty, delete it
                             experiment.getResource().delete(true, progressMonitor);
-                            experiment.getParent().refresh();
                         }
                     }
                 }
