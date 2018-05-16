3090998263f32bc2e16568fe402f8bab1609b086
Geneviève Bastien <gbastien+lttng@versatic.net>
2018-05-16 14:21:18 -0400

    profiling: Add the empty plugins
    It adds the .core, .ui and .core.tests plugins
    
    Change-Id: I6c0634a249e88246b12eec2e06572cc1bf736202
    Signed-off-by: Geneviève Bastien <gbastien+lttng@versatic.net>
    Reviewed-on: https://git.eclipse.org/r/122817
    Tested-by: CI Bot
    Reviewed-by: Matthew Khouzam <matthew.khouzam@ericsson.com>
    Tested-by: Matthew Khouzam <matthew.khouzam@ericsson.com>

---
+++ analysis/org.eclipse.tracecompass.analysis.profiling.ui/src/org/eclipse/tracecompass/internal/analysis/profiling/ui/Activator.java
@@ -0,0 +1,55 @@
+/*******************************************************************************
+ * Copyright (c) 2018 École Polytechnique de Montréal
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+
+package org.eclipse.tracecompass.internal.analysis.profiling.ui;
+
+import org.eclipse.jdt.annotation.Nullable;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class Activator extends AbstractUIPlugin {
+
+    /** The plugin ID */
+    public static final String PLUGIN_ID = "org.eclipse.tracecompass.analysis.profiling.ui"; //$NON-NLS-1$
+
+    // The shared instance
+    private static @Nullable Activator plugin;
+
+    /**
+     * The constructor
+     */
+    public Activator() {
+    }
+
+    @Override
+    public void start(@Nullable BundleContext context) throws Exception {
+        super.start(context);
+        plugin = this;
+    }
+
+    @Override
+    public void stop(@Nullable BundleContext context) throws Exception {
+        plugin = null;
+        super.stop(context);
+    }
+
+    /**
+     * Returns the shared instance
+     *
+     * @return the shared instance
+     */
+    public static @Nullable Activator getDefault() {
+        return plugin;
+    }
+
+}
+
