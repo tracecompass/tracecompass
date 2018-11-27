63be0ae05b8b24871e726f76043ea96a07a5bf9b
Patrick Tasse <patrick.tasse@gmail.com>
2018-11-27 16:06:25 -0500

    swtbot: Fix XYDataProviderBaseTest.verifySeriesStyle()
    In the UI thread, TmfCommonXAxisChartViewer.createSWTSeriesFromModel()
    first creates the series, then sets the series style.
    
    The tests wait for the series to be created (in the test thread) then
    immediately verifies the series style (also in the test thread). At that
    instant the series style may not have been set yet in the UI thread.
    
    The verification method now does a syncExec() on the UI thread to ensure
    that the runnable that created the new series has finished executing.
    
    Change-Id: I768e293e970e1a7bc56fcda9fb03aad396d43ab6
    Signed-off-by: Patrick Tasse <patrick.tasse@gmail.com>
    Reviewed-on: https://git.eclipse.org/r/133148
    Tested-by: CI Bot
    Reviewed-by: Bernd Hufmann <bernd.hufmann@ericsson.com>
    Tested-by: Bernd Hufmann <bernd.hufmann@ericsson.com>

--- tmf/org.eclipse.tracecompass.tmf.ui.swtbot.tests/src/org/eclipse/tracecompass/tmf/ui/swtbot/tests/XYDataProviderBaseTest.java
+++ tmf/org.eclipse.tracecompass.tmf.ui.swtbot.tests/src/org/eclipse/tracecompass/tmf/ui/swtbot/tests/XYDataProviderBaseTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2017 Ericsson
+ * Copyright (c) 2017, 2018 Ericsson
  *
  * All rights reserved. This program and the accompanying materials are
  * made available under the terms of the Eclipse Public License v1.0 which
@@ -29,6 +29,7 @@ import org.eclipse.jdt.annotation.Nullable;
 import org.eclipse.swt.graphics.RGB;
 import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
 import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotView;
+import org.eclipse.swtbot.swt.finder.finders.UIThreadRunnable;
 import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
 import org.eclipse.swtbot.swt.finder.matchers.WidgetOfType;
 import org.eclipse.swtbot.swt.finder.utils.FileUtils;
@@ -256,6 +257,9 @@ public abstract class XYDataProviderBaseTest {
      *            either
      */
     protected void verifySeriesStyle(String seriesName, ISeries.SeriesType expectedType, @Nullable RGB expectedColor, LineStyle expectedLineStyle, boolean isArea) {
+        /* Make sure the UI update is complete */
+        UIThreadRunnable.syncExec(() -> {});
+
         ISeries series = fChart.getSeriesSet().getSeries(seriesName);
         assertNotNull(series);
         assertTrue(series.isVisible());
