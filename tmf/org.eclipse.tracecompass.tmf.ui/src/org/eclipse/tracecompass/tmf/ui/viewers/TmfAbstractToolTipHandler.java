1a8060f2ed0352fdee60d6ed06fb1821187ca682
Matthew Khouzam <matthew.khouzam@ericsson.com>
2019-03-07 13:32:31 -0500

    tmf.ui: don't dispose tooltip when the cursor is over it
    Also ignore small mouse movements using a deadzone.
    
    Change-Id: Iad401776ac146776af13db405ea452469340399b
    Signed-off-by: Matthew Khouzam <matthew.khouzam@ericsson.com>
    Reviewed-on: https://git.eclipse.org/r/138293
    Tested-by: CI Bot
    Reviewed-by: Patrick Tasse <patrick.tasse@gmail.com>
    Tested-by: Patrick Tasse <patrick.tasse@gmail.com>

--- tmf/org.eclipse.tracecompass.tmf.ui/src/org/eclipse/tracecompass/tmf/ui/viewers/TmfAbstractToolTipHandler.java
+++ tmf/org.eclipse.tracecompass.tmf.ui/src/org/eclipse/tracecompass/tmf/ui/viewers/TmfAbstractToolTipHandler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2017 Ericsson
+ * Copyright (c) 2017, 2019 Ericsson
  *
  * All rights reserved. This program and the accompanying materials are
  * made available under the terms of the Eclipse Public License v1.0 which
@@ -10,7 +10,6 @@
 package org.eclipse.tracecompass.tmf.ui.viewers;
 
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.MouseAdapter;
 import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.events.MouseTrackAdapter;
 import org.eclipse.swt.graphics.Point;
@@ -20,7 +19,9 @@ import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.tracecompass.tmf.ui.widgets.timegraph.widgets.TimeGraphTooltipHandler;
 
@@ -31,9 +32,51 @@ import org.eclipse.tracecompass.tmf.ui.widgets.timegraph.widgets.TimeGraphToolti
  * @author Loic Prieur-Drevon - extracted from {@link TimeGraphTooltipHandler}
  */
 public abstract class TmfAbstractToolTipHandler {
+    private static final int MOUSE_DEADZONE = 5;
     private static final int OFFSET = 16;
     private Composite fTipComposite;
     private Shell fTipShell;
+    private Rectangle fInitialDeadzone;
+
+    /**
+     * Important note: this is being added to a display filter, this may leak,
+     * make sure it is removed when not needed.
+     */
+    private final Listener fListener = this::disposeIfExited;
+    private final Listener fFocusLostListener = event -> {
+        Shell tipShell = fTipShell;
+        // Don't dispose if the tooltip is clicked.
+        if (tipShell != null && event.display.getActiveShell() != tipShell) {
+            tipShell.dispose();
+        }
+    };
+
+    /**
+     * Dispose the shell if we exit the range.
+     *
+     * @param e
+     *            The event which occurred
+     */
+    private void disposeIfExited(Event e) {
+        if (!(e.widget instanceof Control)) {
+            return;
+        }
+        Control control = (Control) e.widget;
+        if (control != null && !control.isDisposed()) {
+            Point pt = control.toDisplay(e.x, e.y);
+            Shell tipShell = fTipShell;
+            if (tipShell != null && !tipShell.isDisposed()) {
+                Rectangle bounds = tipShell.getBounds();
+                bounds.x -= OFFSET;
+                bounds.y -= OFFSET;
+                bounds.height += 2 * OFFSET;
+                bounds.width += 2 * OFFSET;
+                if (!bounds.contains(pt) && !fInitialDeadzone.contains(pt)) {
+                    tipShell.dispose();
+                }
+            }
+        }
+    }
 
     /**
      * Callback for the mouse-over tooltip
@@ -42,38 +85,21 @@ public abstract class TmfAbstractToolTipHandler {
      *            The control object to use
      */
     public void activateHoverHelp(final Control control) {
-        control.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseDown(MouseEvent e) {
-                if (fTipShell != null && !fTipShell.isDisposed()) {
-                    fTipShell.dispose();
-                }
-            }
-        });
-
-        control.addMouseMoveListener(e -> {
-            if (fTipShell != null && !fTipShell.isDisposed()) {
-                fTipShell.dispose();
-            }
-        });
 
         control.addMouseTrackListener(new MouseTrackAdapter() {
             @Override
-            public void mouseExit(MouseEvent e) {
-                Point pt = control.toDisplay(e.x, e.y);
-                if (fTipShell != null && !fTipShell.isDisposed()
-                        && !fTipShell.getBounds().contains(pt)) {
-                    fTipShell.dispose();
-                }
-            }
-
-            @Override
             public void mouseHover(MouseEvent event) {
-                if ((event.stateMask & SWT.BUTTON_MASK) != 0) {
+                // Is application not in focus?
+                // -OR- a mouse button is pressed
+                if (Display.getDefault().getFocusControl() == null
+                        || (event.stateMask & SWT.BUTTON_MASK) != 0
+                        || (event.stateMask & SWT.KEY_MASK) != 0) {
                     return;
                 }
                 Point pt = new Point(event.x, event.y);
                 Control timeGraphControl = (Control) event.widget;
+                Point ptInDisplay = control.toDisplay(event.x, event.y);
+                fInitialDeadzone = new Rectangle(ptInDisplay.x - MOUSE_DEADZONE, ptInDisplay.y - MOUSE_DEADZONE, 2 * MOUSE_DEADZONE, 2 * MOUSE_DEADZONE);
                 createTooltipShell(timeGraphControl.getShell());
                 for (Control child : fTipComposite.getChildren()) {
                     child.dispose();
@@ -87,6 +113,10 @@ public abstract class TmfAbstractToolTipHandler {
                 Point tipPosition = control.toDisplay(pt);
                 setHoverLocation(fTipShell, tipPosition);
                 fTipShell.setVisible(true);
+                // Register Display filters.
+                Display display = Display.getDefault();
+                display.addFilter(SWT.MouseMove, fListener);
+                display.addFilter(SWT.FocusOut, fFocusLostListener);
             }
         });
     }
@@ -97,13 +127,20 @@ public abstract class TmfAbstractToolTipHandler {
             fTipShell.dispose();
         }
         fTipShell = new Shell(parent, SWT.ON_TOP | SWT.TOOL);
+        // Deregister display filters on dispose
+        fTipShell.addDisposeListener(e -> e.display.removeFilter(SWT.MouseMove, fListener));
+        fTipShell.addDisposeListener(e -> e.display.removeFilter(SWT.FocusOut, fFocusLostListener));
+        fTipShell.addListener(SWT.Deactivate, e -> {
+            if (!fTipShell.isDisposed()) {
+                fTipShell.dispose();
+            }
+        });
         GridLayout gridLayout = new GridLayout();
         gridLayout.numColumns = 2;
         gridLayout.marginWidth = 2;
         gridLayout.marginHeight = 2;
         fTipShell.setLayout(gridLayout);
         fTipShell.setBackground(display.getSystemColor(SWT.COLOR_INFO_BACKGROUND));
-
         fTipComposite = new Composite(fTipShell, SWT.NONE);
         fTipComposite.setLayout(new GridLayout(3, false));
         setupControl(fTipComposite);
@@ -128,22 +165,6 @@ public abstract class TmfAbstractToolTipHandler {
     private void setupControl(Control control) {
         control.setForeground(fTipShell.getDisplay().getSystemColor(SWT.COLOR_INFO_FOREGROUND));
         control.setBackground(fTipShell.getDisplay().getSystemColor(SWT.COLOR_INFO_BACKGROUND));
-
-        control.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseDown(MouseEvent e) {
-                fTipShell.dispose();
-            }
-        });
-
-        control.addMouseTrackListener(new MouseTrackAdapter() {
-            @Override
-            public void mouseExit(MouseEvent e) {
-                fTipShell.dispose();
-            }
-        });
-
-        control.addMouseMoveListener(e -> fTipShell.dispose());
     }
 
     /**
