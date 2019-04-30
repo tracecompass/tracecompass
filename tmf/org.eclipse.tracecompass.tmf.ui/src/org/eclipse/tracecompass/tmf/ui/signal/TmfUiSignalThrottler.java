e3104d95f83bbe2cfbd88b7192bc9a8920c94e9c
Patrick Tasse <patrick.tasse@gmail.com>
2019-04-30 15:44:35 -0400

    tmf: Bug 546878: Deadlock in TmfTimeViewer
    Add TmfUiSignalThrottler class to broadcast signals from the UI thread.
    
    Change-Id: I6bc8c61fe51bdfd7eaadaa1067c68c2263b87dd7
    Signed-off-by: Patrick Tasse <patrick.tasse@gmail.com>
    Reviewed-on: https://git.eclipse.org/r/141427
    Reviewed-by: Matthew Khouzam <matthew.khouzam@ericsson.com>
    Tested-by: Matthew Khouzam <matthew.khouzam@ericsson.com>
    Tested-by: CI Bot

---
+++ tmf/org.eclipse.tracecompass.tmf.ui/src/org/eclipse/tracecompass/tmf/ui/signal/TmfUiSignalThrottler.java
@@ -0,0 +1,63 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Ericsson
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Patrick Tasse - Initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.tracecompass.tmf.ui.signal;
+
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.tracecompass.tmf.core.component.ITmfComponent;
+import org.eclipse.tracecompass.tmf.core.signal.TmfSignal;
+import org.eclipse.tracecompass.tmf.core.signal.TmfSignalManager;
+import org.eclipse.tracecompass.tmf.core.signal.TmfSignalThrottler;
+
+/**
+ * "Buffer" between a TmfComponent and the signal manager. You can use this if
+ * you want to throttle the amount of signals your component will send.
+ * <p>
+ * It works by specifying a delay, then calling {@link #queue}. The signals will
+ * only be really sent if no other call to {@link #queue} happens within $delay
+ * milliseconds afterwards. This guarantees that only the *last* signal is
+ * actually broadcasted.
+ * <p>
+ * Note that this class does not discriminate for signal types, sources, or
+ * whatever. If you want to throttle different signals in different ways, you
+ * can use multiple signal throttlers in your component and call them
+ * accordingly.
+ * <p>
+ * Unlike {@link TmfSignalThrottler}, this throttler will broadcast
+ * the signal from the UI thread.
+ *
+ * @since 5.0
+ */
+public class TmfUiSignalThrottler extends TmfSignalThrottler {
+
+    /**
+     * Constructor
+     *
+     * @param component
+     *            The optional source component of the signals. If non-null, its
+     *            {@link ITmfComponent#broadcast} method will be used to finally
+     *            send the signal. If null, the generic
+     *            {@link TmfSignalManager#dispatchSignal} is used.
+     * @param delay
+     *            Time to wait before actually sending signals (in ms)
+     */
+    public TmfUiSignalThrottler(@Nullable ITmfComponent component, long delay) {
+        super(component, delay);
+    }
+
+    @Override
+    protected void dispatchSignal(@NonNull TmfSignal signal) {
+        Display.getDefault().asyncExec(() -> super.dispatchSignal(signal));
+    }
+}
