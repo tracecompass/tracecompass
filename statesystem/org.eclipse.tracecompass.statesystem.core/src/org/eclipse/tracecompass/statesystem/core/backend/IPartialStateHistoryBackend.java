a8188ed2400f4a9b42406fd0bb47a33a39914081
Marco Miller <marco.miller@ericsson.com>
2022-06-15 16:13:29 -0400

    statesystem.core: Bump IPartialStateHistoryBackend
    Fix the build errors in Eclipse that showed after commit 0e7f5b1bb4.
    
    Bump the related minor versions as recommended by those Quick Fixes.
    
    Change-Id: I84f9aaa3295619b76daad8e8bc16b314220bd35e
    Signed-off-by: Marco Miller <marco.miller@ericsson.com>
    Reviewed-on: https://git.eclipse.org/r/c/tracecompass/org.eclipse.tracecompass/+/194201
    Tested-by: Matthew Khouzam <matthew.khouzam@ericsson.com>
    Tested-by: Trace Compass Bot <tracecompass-bot@eclipse.org>
    Reviewed-by: Matthew Khouzam <matthew.khouzam@ericsson.com>
    Reviewed-by: Bernd Hufmann <bernd.hufmann@ericsson.com>

--- statesystem/org.eclipse.tracecompass.statesystem.core/src/org/eclipse/tracecompass/statesystem/core/backend/IPartialStateHistoryBackend.java
+++ statesystem/org.eclipse.tracecompass.statesystem.core/src/org/eclipse/tracecompass/statesystem/core/backend/IPartialStateHistoryBackend.java
@@ -19,7 +19,7 @@ import org.eclipse.tracecompass.internal.provisional.datastore.core.condition.Ti
  * specific for the partial state system.
  *
  * @author Abdellah Rahmani
- * @since 5.1
+ * @since 5.2
  */
 public interface IPartialStateHistoryBackend extends IStateHistoryBackend {
 
